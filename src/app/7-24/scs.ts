9186b0093ed98ec360d1f51e69cca9dd1a5b3fc4
subh <harvirbuttar2917@gmail.com>
2024-07-09 16:37:49 -0700

    init shortest super subsequence

---
+++ src/app/7-24/scs.ts
@@ -0,0 +1,20 @@
+// Byimaan
+
+/**
+ * Given two strings str1 and str2, the task is to find the length of the shortest string that has both str1 and str2 as subsequences.
+ * 
+ * Input:   str1 = "geek",  str2 = "eke"
+    Output: 5
+    Explanation: 
+    String "geeke" has both string "geek" 
+    and "eke" as subsequences.
+
+    Input:   str1 = "AGGTAB",  str2 = "GXTXAYB"
+    Output:  9
+    Explanation: 
+    String "AGXGTXAYB" has both string 
+    "AGGTAB" and "GXTXAYB" as subsequences
+
+    CMD npx ts-node ./src/app/7-24/scs.ts
+
+ */
