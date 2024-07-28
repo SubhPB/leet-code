c51652e0df60238bc897733464e19134e652012b
subh <harvirbuttar2917@gmail.com>
2024-07-28 15:55:15 -0700

    completed recusion approach

--- src/app/7-24/longest-path-in-matrix.ts
+++ src/app/7-24/longest-path-in-matrix.ts
@@ -14,12 +14,15 @@ Input: matrix = [
 ]
 Output: 4
 Explanation: The longest increasing path is [1, 2, 6, 9]
+
+
+CMD :- npx ts-node ./src/app/7-24/longest-path-in-matrix.ts
  */
 
 const Matrix = [
-    [9,9,4],
-    [6,6,8],
-    [2,1,1]
+    [3, 4, 5],
+    [4, 7, 6],
+    [5 ,6, 10]
 ]
 
 function findLongestpathInMatrix(matrix=Matrix){
@@ -31,19 +34,38 @@ function findLongestpathInMatrix(matrix=Matrix){
         [0, -1], // left
     ];
 
+    let longestPath: number[] = [];
 
-    const longestPath = [];
-
-    const solve = (i: number, j: number) => {
-        const queue = [matrix[i][j]]
+    const isValid = (i:number, j:number, currVal: number) => {
+        return (
+            i >= 0 && i < matrix.length && j >=0 && j < matrix[0].length && currVal < matrix[i][j]
+        )
+    };
+    
+    const recursion = (i: number, j: number, path: number[]) => {
         
-        while (queue.length !== 0){
+        const validDirections = directions.filter(dir => isValid(dir[0] + i, dir[1] + j,  matrix[i][j]));
 
-            for(let dir of directions){
-
+        if (validDirections.length === 0 ){
+            if (path.length + 1 > longestPath.length){
+                longestPath = [...path, matrix[i][j]]
+            };
+        } else {
+            for(let dir of validDirections){
+                recursion(
+                    dir[0]+i, dir[1]+j, [...path, matrix[i][j]]
+                )
             }
         }
+    };
+
+    for (let i = 0;  i < matrix.length; i++){
+        for(let j=0; j< matrix[0].length; j++) {
+            recursion(i, j, [])
+        }
     }
+
+    return longestPath
 
 };
 
