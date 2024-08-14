dd560208f1f67795a7b208844035b4afdaf03a93
subh <harvirbuttar2917@gmail.com>
2024-08-13 17:24:41 -0700

    ...

--- src/app/8-24/find-substring-30.ts
+++ src/app/8-24/find-substring-30.ts
@@ -23,16 +23,6 @@ Explanation:
 The substring starting at 0 is "barfoo". It is the concatenation of ["bar","foo"] which is a permutation of words.
 The substring starting at 9 is "foobar". It is the concatenation of ["foo","bar"] which is a permutation of words.
 
-Example 2:
-
-Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
-
-Output: []
-
-Explanation:
-
-There is no concatenated substring.
-
 Example 3:
 
 Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
