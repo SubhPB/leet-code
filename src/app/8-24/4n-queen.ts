139a4e8ef682284c0333583ca6d23964fd050d50
subh <harvirbuttar2917@gmail.com>
2024-08-21 16:58:30 -0700

    [#4n-queen:solution] Solved problem using backttracking

--- src/app/8-24/4n-queen.ts
+++ src/app/8-24/4n-queen.ts
@@ -5,13 +5,47 @@
  * CMD - npx ts-node ./src/app/8-24/4n-queen.ts
  */
 
+class Queen {
+    constructor(public x: number, public y:number){
+        this.x = x;
+        this.y = y;
+    };
 
-function placeQueen(n=4){
-    const matrix = Array.from({length: 4}, () => Array.from({length: 4}, () => '.'))
-    for(let r of matrix){
-        console.log(r)
+    /** x and y is the location of other chess piece */
+    isUnderAttack(x:number, y: number){
+        const verticalOrHorizontalLineUnderAttck = this.x === x || this.y === y
+        const diagonalsUnderAttack = /** bottomLeftToTopright || topLeftToBottomRight */ this.x + this.y === x + y || this.x - this.y === x - y
+        return verticalOrHorizontalLineUnderAttck || diagonalsUnderAttack
     }
 
+}
+
+function placeQueen(n=4){
+    const matrix = Array.from({length: 4}, () => Array.from({length: 4}, () => ' '));
+    
+    const sols:number[][][] = []
+
+    const backTrack = (row: number, paths: number[][]) => {
+        if (paths.length === n){
+            console.log("HELO!")
+            sols.push(paths)
+            return;
+        };
+        
+        if (row >= n){
+            return
+        }
+        
+        for (let col = 0; col < n; col++){
+            /** x will be col and y will be row because col moves in horizonal fashion and row moves in vertical fashion  */
+            const isSafe = paths.every(([x, y] )=> !(new Queen(col, row).isUnderAttack(y, x)));
+            if (isSafe){
+                backTrack(row+1, [...paths, [row, col]])
+            }
+        }
+    };
+    backTrack(0, [])
+    return sols
 };
 
 console.log(placeQueen())
