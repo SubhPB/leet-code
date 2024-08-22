106d24125dc0d12261149ba0ede34392441e1ab8
subh <harvirbuttar2917@gmail.com>
2024-08-22 15:51:48 -0700

    [#388:longest-absolute-path] init the problem statement

---
+++ src/app/8-24/lng-abs-path.ts
@@ -0,0 +1,36 @@
+/**
+ * Byimaan
+ * 
+ * 388. Longest Absolute File Path
+Companies
+Suppose we have a file system that stores both files and directories. An example of one system is represented in the following picture:
+
+Here, we have dir as the only directory in the root. dir contains two subdirectories, subdir1 and subdir2. subdir1 contains a file file1.ext and subdirectory subsubdir1. subdir2 contains a subdirectory subsubdir2, which contains a file file2.ext.
+
+In text form, it looks like this (with ⟶ representing the tab character):
+
+dir
+⟶ subdir1
+⟶ ⟶ file1.ext
+⟶ ⟶ subsubdir1
+⟶ subdir2
+⟶ ⟶ subsubdir2
+⟶ ⟶ ⟶ file2.ext
+
+Input: input = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+Output: 32
+Explanation: We have two files:
+"dir/subdir1/file1.ext" of length 21
+"dir/subdir2/subsubdir2/file2.ext" of length 32.
+We return 32 since it is the longest absolute path to a file.
+
+ CMD :- npx ts-node ./src/app/8-24/lng-abs-path.ts
+ */
+
+function longestAbsPath(input = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"){
+
+    let lngAbsPath = '';
+    /** solution here */
+    return lngAbsPath
+}
+
