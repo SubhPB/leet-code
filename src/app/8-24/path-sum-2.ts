c0b05589873d0e9ef26626d3b8514d7f7067944d
subh <harvirbuttar2917@gmail.com>
2024-08-31 15:26:20 -0700

    #113 | path-sum-II need to fix a bug in the solution

--- src/app/8-24/path-sum-2.ts
+++ src/app/8-24/path-sum-2.ts
@@ -25,11 +25,40 @@ Example 3:
 
 Input: root = [1,2], targetSum = 0
 Output: []
+
+CMD - npx ts-node ./src/app/8-24/path-sum-2.ts
  */
 
-function pathSum(root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22){
+function pathSum(root =[5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22){
 
-    const paths:number[] = [];
+    const paths:number[][] = [];
+
+    const isLeaf = (i:number) => i >= root.length || root[i] === null
+
+    const BT = (index: number, sum: number, path: NonNullable<typeof root[number]>[]) => {
+
+        if (index < 0){
+            return
+        }
+
+        if (isLeaf(index) && sum === targetSum){
+            paths.push([...path]);
+            return
+        }
+        
+        const currNode = root?.[index], leftInd = (2*index) + 1, rightInd = 2*(index+1);
+
+        if(typeof currNode === 'number'){
+            let newPath = [...path, currNode];
+            // leftIndex
+            BT(leftInd, sum + currNode, [...newPath]);
+            BT(rightInd, sum + currNode, [...newPath])
+        }
+    };
+
+    BT(0, 0, [])
 
     return paths
-}
+};
+
+console.log(pathSum())
