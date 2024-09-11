a63c785e42a62d1568d4590c46a4d68b0414a5b1
subh <harvirbuttar2917@gmail.com>
2024-09-11 16:42:05 -0700

    [#915 | partition | solved] problem solved

--- src/app/9-24/partition-arr-915.ts
+++ src/app/9-24/partition-arr-915.ts
@@ -26,10 +26,18 @@
 
 
 function paritionDisjoint(nums = [5,0,3,8,6]){
-    let rightArrIndex = 0;
+    if (!nums.length) return [];
     /** solution logic here */
-
-    return nums.slice(0, rightArrIndex)
+    
+    for(let i = 0; i < nums.length; i++){
+        const leftArr = nums.slice(0, i), rightArr = nums.slice(i, nums.length);
+        if (leftArr.length && leftArr.every( ln => rightArr.every(rn => rn >= ln))){
+            return [
+                leftArr,
+                rightArr
+            ]
+        }
+    }
 };
 
 console.log(paritionDisjoint())
