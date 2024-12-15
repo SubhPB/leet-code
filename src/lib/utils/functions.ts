a685b8102b82c1d244e01d170e759c1adb29827c
subh <harvirbuttar2917@gmail.com>
2024-12-15 17:38:36 -0600

    A util fn to convert +ve int into binary

--- src/lib/utils/functions.ts
+++ src/lib/utils/functions.ts
@@ -2,4 +2,18 @@
 
 export function deepCopy<T=any, R=any>(arg: T): R{
     return JSON.parse(JSON.stringify(arg))
+};
+
+export function positiveIntToBinary(int:number){
+    if (int <= 0){
+        if (int<0) throw new Error(`Expected positive integer but got ${int}`);
+        return '0'
+    };
+    let binary = ''
+    while (int!==0){
+        const remainder = int%2;
+        int = Math.floor(int/2);
+        binary = remainder.toString() + binary
+    };
+    return binary
 }
