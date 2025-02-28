ec096728ff26463d2564efa92052167d303db3e1
subh <harvirbuttar2917@gmail.com>
2025-02-27 18:25:18 -0600

    Straight-forward solution of leetcode-136

---
+++ src/app/2025/Feb/136.ts
@@ -0,0 +1,49 @@
+/**
+ * 136. Single Number
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+
+Input: nums = [2,2,1]
+
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+
+Output: 1
+npx ts-node ./src/app/2025/Feb/136.ts
+ */
+
+class Solve136{
+    constructor(public nums:number[]){this.nums=nums};
+    solution(nums=this.nums){
+        const dict: {[k:string]:any} = {};
+        for(let num of nums){
+            if (num in dict) delete dict[num];
+            else dict[num] = true
+        };
+        return parseInt(Object.keys(dict)[0])
+    }
+};
+(
+    ()=>{
+        const ARGS = [
+            [2,2,1],
+            [4,1,2,1,2],
+            [1]
+        ];
+
+        ARGS.forEach(nums =>console.log(`nums=[${nums.join(', ')}], Unique Int = ${new Solve136(nums).solution()}`))
+        
+    }
+)()
