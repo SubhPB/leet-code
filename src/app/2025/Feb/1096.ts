73a8df641e67354b19f934be3251816ff22f77f4
subh <harvirbuttar2917@gmail.com>
2025-03-03 17:20:14 -0600

    tried to way to solve 1096 but not successful

--- src/app/2025/Feb/1096.ts
+++ src/app/2025/Feb/1096.ts
@@ -38,15 +38,78 @@ Constraints:
 1 <= expression.length <= 60
 expression[i] consists of '{', '}', ','or lowercase English letters.
 The given expression represents a set of words based on the grammar given in the description.
+
+ npx ts-node ./src/app/2025/Feb/1096.ts
  */
 
 class Solve1096{
     constructor(public expression:string){
         this.expression=expression
     };
-    solution(expr=this.expression){
-        const n = expr.length;
-        if (n<=1) return [expr];
-        return [expr]
+    solution(expression=this.expression){
+        class Dict{
+            constructor(public expr:string){
+                this.expr = expr
+            };
+            flat(){
+                if (this.expr.length<=1) return `{${this.expr}}`;
+
+                const tempStartStack = [0];
+                const startAt:number[] = [], endsAt:number[] = [];
+
+                for(let i=1; i<this.expr.length; i++){
+                    if (this.expr[i]==='{'){
+                        tempStartStack.push(i)
+                    } else if (this.expr==='}'){
+                        startAt.push(tempStartStack.pop()!);
+                        endsAt.push(i);
+                    } else {
+                        //If char is not been surrounded by any bracket e.g. `f` in here: {a{b,c}f{d}}
+                        startAt.push(i); endsAt.push(i); //A special way to represent this,
+                    }
+                };
+
+                let flattenExpr = '{';
+                let i = 1;
+                while(i<startAt.length){
+                    if (startAt[i]===endsAt[i]) flattenExpr += `{${this.expr[i]}}`;
+                    else {
+                        const subExpr = this.expr.substring(startAt[i], endsAt[i]+1);
+                        const isDeeplyNested = (currI:number) => currI+1<startAt.length && endsAt[currI]>startAt[currI+1];
+                        if (isDeeplyNested(i)){
+                            flattenExpr += new Dict(subExpr).flat();
+                            while (isDeeplyNested(i)) i++
+                        }
+                    };
+                    i++
+                }
+                flattenExpr += '}'
+                return flattenExpr 
+                
+            };
+            // parse(){
+            //     const flattenExpr = this.flat(); //Here we sure that expression is flatten and not deeply nested
+            //     const stack: string[][] = [];
+            //     for(let i=1; i<flattenExpr.length-1; i++){
+            //         if (flattenExpr[i]==='{'){
+
+            //         }
+            //     }
+            // }
+            
+        };
+        return new Dict(expression)
     }
-}
+};
+
+(
+    ()=>{
+        const Expressions = [
+            "{a,b}{c,{d,e}}",
+            "{{a,z},a{b,c},{ab,z}}",
+            "a{b,c}{d,e}f{g,h}",
+            "{a,b}{c,d}",
+        ];
+        Expressions.forEach(exp => console.log(`expression=(${exp}) testExpression: ${new Solve1096(exp).solution().flat()}`))
+    }
+)()
