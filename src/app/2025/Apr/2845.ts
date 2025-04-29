9825856ecdb94e68c8820bab8fca5d0cd5645d62
subh <harvirbuttar2917@gmail.com>
2025-04-29 11:13:36 -0600

    finally grasped the logic after days of starring the algorithm,was uite counter-intuitive at first

--- src/app/2025/Apr/2845.ts
+++ src/app/2025/Apr/2845.ts
@@ -45,23 +45,24 @@ class Solve2845{
     constructor(public nums:number[], public mod:number, public k:number){
         this.nums=nums; this.mod=mod; this.k=k
     };
-    solution(nums=this.nums, mod=this.mod, k=this.k){
+    /**Solution1 not works! */
+    solution1(nums=this.nums, mod=this.mod, k=this.k){
         const n = nums.length;
         const table:number[] = [];
         const calc = (l:number) => l*(l+1)/2
         for(let i=0; i<n; i++){
             if (nums[i] % mod === k) table.push(i);
         };
-        console.log({table})
+        // console.log({table})
         let res = 0, cnt = k;
         while(cnt<=table.length){
             if (cnt===0){
                 if (table.length===0) {
                     return calc(n)
                 } else {
-                    for(let i=0; i<table.length-1; i++){
+                    for(let i=-1; i<table.length-1; i++){
                         const len = (
-                            table[i+1] - table[i] - 1
+                            (table[i+1]) - (i>=0 ? table[i] : -1) - 1
                         );
                         res += calc(len)
                     }
@@ -77,6 +78,22 @@ class Solve2845{
             cnt += mod
         }
         return res
+    };
+    solution2(nums=this.nums, modulo=this.mod, k=this.k){
+        let cnt = 0, res = 0;
+        const map : {[k:number]:number} = {0:1};
+        const get = (k:number) => k in map ? map[k] : 0;
+
+        for(let num of nums){
+            cnt += (
+                num%modulo===k ? 1 : 0
+            );
+            cnt %= modulo; //e.g. 4%3 and 1%3 both same
+            const oldCnt = (cnt - k + modulo)%modulo;
+            res += get(oldCnt);
+            map[cnt] = 1 + get(cnt);
+        };
+        return res;
     }
 };
 
@@ -85,13 +102,17 @@ class Solve2845{
         const ARGS: [number[], number, number][] = [
             [[3,2,4], 2, 1],
             [[3,1,9,6], 3, 0],
-            [[2,7], 7, 0]
+            [[2,7], 7, 0],
+            [[5,1], 6, 1],
+            [[7,2], 7, 0],
+            [[2,1,5], 9, 1],
+            [[2,2,5], 3, 2]
         ];
 
         ARGS.forEach(
             ([nums, mod, k]) => {
                 const sol = new Solve2845(nums,mod,k);
-                console.log(`Nums=[${nums.join(',')}] Mod=${mod} K=${k} Ans=${sol.solution()}`)
+                console.log(`Nums=[${nums.join(',')}] Mod=${mod} K=${k} Ans=${sol.solution1()}`)
             }
         )
     }
