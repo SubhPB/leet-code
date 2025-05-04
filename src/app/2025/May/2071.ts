a5e2f204639a972b46263eae2802d7b60e03fe73
subh <harvirbuttar2917@gmail.com>
2025-05-03 18:28:03 -0600

    Understood and implemented the algorithm efficiently

--- src/app/2025/May/2071.ts
+++ src/app/2025/May/2071.ts
@@ -42,6 +42,8 @@ m == workers.length
 1 <= n, m <= 5 * 104
 0 <= pills <= m
 0 <= tasks[i], workers[j], strength <= 109
+
+npx ts-node ./src/app/2025/May/2071.ts
  */
 
 class Solve2071{
@@ -54,23 +56,96 @@ class Solve2071{
     solution(
         tasks=this.tasks, workers=this.workers, pills=this.pills, strength=this.strength
     ){
-        let res = 0;
-        const m = workers.length, n = tasks.length;
-        const tasksGraph:{[k:number]:number} = {};
-        for(let task of tasks){
-            if (!(task in tasksGraph)) tasksGraph[task] = 0;
-            tasksGraph[task]++;
-        };
-        const workersGraph: {[k:number]:number} = {}
-        for(let worker of workers){
-            if (!(worker in workersGraph)) workersGraph[worker] = 0;
-            if (worker in tasksGraph){
-                tasksGraph[worker]--;
-                res++;
-                if (tasksGraph[worker]===0) delete tasksGraph[worker];
-                if (workersGraph[worker]===0) delete workersGraph[worker]
-            } else workersGraph[worker]++;
-        };
-        return res
+        const n = tasks.length, m = workers.length;
+
+        const sortedTasks = tasks.sort((a,b)=>a-b);
+        const sortedWorkers = workers.sort((a,b)=>b-a);
+        let needToSortWorkers = false;
+
+        const canAssign = (cnt:number) => {
+            if ([m,n].every(l=>cnt>=0&&cnt<=l)){
+                /**
+                 * Process:-
+                 * 1) Assign strongest worker the hardest task (within cnt range)
+                 * 2) If pill needed, elect optimal worker to use pill wisely.
+                 * 3) If can't do any task in within range then return false immediately 
+                 */
+                if (needToSortWorkers) sortedWorkers.sort((a,b) => b-a)
+                let pillsCnt = pills;
+                for(let i=0; i<cnt; i++){
+                    const suppl = strength * Math.min(1, pillsCnt);
+                    const task = sortedTasks[cnt-i-1];
+
+                    if (sortedWorkers[i] + suppl >= task){
+
+                        if (sortedWorkers[i] < task){ //Does pill needed?
+                            //Need to elect most optimal worker
+                            let l = i, r = cnt-1;
+                            while(l<r){
+                                let candid = Math.ceil( (l+r)/2 );
+                                if (
+                                    sortedWorkers[candid] + suppl >= task
+                                ){ //A weak worker is found, capable to complete this task
+                                    l=candid;
+                                } else {
+                                    r=candid-1;
+                                }
+                            };
+                            //So now sortedWorker['l'] is most optimal candidate to elect
+                            if (i!==l){
+                                needToSortWorkers = true;
+                                while(i!==l){
+                                    [sortedWorkers[l-1], sortedWorkers[l]] = [sortedWorkers[l], sortedWorkers[l-1]];
+                                    l-=1
+                                }
+                            };
+                            pillsCnt -= 1; //pillUsed
+                        };
+
+                    } else {
+                        return false
+                    }
+                };
+                return true
+            }
+            return false
+        }
+
+        let l=0, r=n;
+        while(l<r){
+            const cnt = Math.ceil(
+                (l+r)/2 //Not using floor because [cnt=0] is always a 'true' answer
+            );
+            if (canAssign(cnt)){
+                l=cnt;
+            } else {
+                r=cnt-1;
+            }
+        }
+        return l
     }
-}
+};
+
+(
+    ()=>{
+        const ARGS: [number[],number[],number,number][] = [
+            [[3,2,1], [0,3,3], 1, 1],
+            [[5,4], [0,0,0], 1, 5],
+            [[5,9,8,5,9], [1,6,4,2,6], 1, 5]
+        ];
+
+        ARGS.forEach(
+            ([tasks,workers,pills,strength]) => {
+                const sol = new Solve2071(tasks,workers,pills,strength);
+                const toStr = (arr:number[]) => `[${arr.join(',')}]`
+                console.log(
+                    `Tasks = ${toStr(tasks)} \n`
+                    + `Workers = ${toStr(workers)} \n`
+                    + `Pills = ${pills} \n`
+                    + `Strength = ${strength} \n`
+                    + `Max-Tasks(1) = ${sol.solution()} \n`
+                )
+            }
+        )
+    }
+)()
