4f81473583d0fd853f6c884669d3f08565ddf57a
subh <harvirbuttar2917@gmail.com>
2025-06-06 19:05:54 -0600

    A reasonable efficient solution of #2434 problem

---
+++ src/app/2025/June/2434.py
@@ -0,0 +1,69 @@
+'''
+2434. Using a Robot to Print the Lexicographically Smallest String
+
+You are given a string s and a robot that currently holds an empty string t. Apply one of the following operations until s and t are both empty:
+
+Remove the first character of a string s and give it to the robot. The robot will append this character to the string t.
+Remove the last character of a string t and give it to the robot. The robot will write this character on paper.
+Return the lexicographically smallest string that can be written on the paper.
+
+Example 1:
+
+Input: s = "zza"
+Output: "azz"
+Explanation: Let p denote the written string.
+Initially p="", s="zza", t="".
+Perform first operation three times p="", s="", t="zza".
+Perform second operation three times p="azz", s="", t="".
+Example 2:
+
+Input: s = "bac"
+Output: "abc"
+Explanation: Let p denote the written string.
+Perform first operation twice p="", s="c", t="ba". 
+Perform second operation twice p="ab", s="c", t="". 
+Perform first operation p="ab", s="", t="c". 
+Perform second operation p="abc", s="", t="".
+Example 3:
+
+Input: s = "bdda"
+Output: "addb"
+Explanation: Let p denote the written string.
+Initially p="", s="bdda", t="".
+Perform first operation four times p="", s="", t="bdda".
+Perform second operation four times p="addb", s="", t="".
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only English lowercase letters.
+'''
+from collections import deque
+
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        idx = lambda x:ord(x)-97
+        count = [0]*26 #It basically count the number of any char left in 's' string
+        pq = deque(sorted(list(s), key=idx))
+
+        for i in range(len(s)): count[idx(s[i])]+=1
+        
+        i, t = 0, []
+
+        res = []
+        while i<len(s):
+            while len(pq) and count[idx(pq[0])]<=0:
+                pq.popleft()
+
+            if len(t) and t[-1] <= pq[0]:
+                res.append(t.pop())
+            else:
+                char = s[i]
+                count[idx(char)]-=1
+                t.append(char)
+                i+=1
+
+
+        while len(t): res.append(t.pop())
+        return ''.join(res)
