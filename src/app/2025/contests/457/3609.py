12a39a77380481177190ddffdc4bb3b1525a71b9
subh <harvirbuttar2917@gmail.com>
2025-07-09 12:50:42 -0600

    Nice problem! solved by graph uplifting (down to up approach)

---
+++ src/app/2025/contests/457/3609.py
@@ -0,0 +1,106 @@
+'''
+3609. Minimum Moves to Reach Target in Grid
+
+    You are given four integers sx, sy, tx, and ty, representing two points (sx, sy) and (tx, ty) on an infinitely large 2D grid.
+
+    You start at (sx, sy).
+
+    At any point (x, y), define m = max(x, y). You can either:
+
+    Move to (x + m, y), or
+    Move to (x, y + m).
+    Return the minimum number of moves required to reach (tx, ty). If it is impossible to reach the target, return -1.
+
+    
+
+    Example 1:
+
+    Input: sx = 1, sy = 2, tx = 5, ty = 4
+
+    Output: 2
+
+    Explanation:
+
+    The optimal path is:
+
+    Move 1: max(1, 2) = 2. Increase the y-coordinate by 2, moving from (1, 2) to (1, 2 + 2) = (1, 4).
+    Move 2: max(1, 4) = 4. Increase the x-coordinate by 4, moving from (1, 4) to (1 + 4, 4) = (5, 4).
+    Thus, the minimum number of moves to reach (5, 4) is 2.
+
+    Example 2:
+
+    Input: sx = 0, sy = 1, tx = 2, ty = 3
+
+    Output: 3
+
+    Explanation:
+
+    The optimal path is:
+
+    Move 1: max(0, 1) = 1. Increase the x-coordinate by 1, moving from (0, 1) to (0 + 1, 1) = (1, 1).
+    Move 2: max(1, 1) = 1. Increase the x-coordinate by 1, moving from (1, 1) to (1 + 1, 1) = (2, 1).
+    Move 3: max(2, 1) = 2. Increase the y-coordinate by 2, moving from (2, 1) to (2, 1 + 2) = (2, 3).
+    Thus, the minimum number of moves to reach (2, 3) is 3.
+
+    Example 3:
+
+    Input: sx = 1, sy = 1, tx = 2, ty = 2
+
+    Output: -1
+
+    Explanation:
+
+    It is impossible to reach (2, 2) from (1, 1) using the allowed moves. Thus, the answer is -1.
+    
+
+    Constraints:
+
+    0 <= sx <= tx <= 10^9
+    0 <= sy <= ty <= 10^9
+
+    python ./src/app/2025/contests/457/3609.py
+
+'''
+
+class Solution:
+    def minMoves(self, sx: int, sy: int, tx: int, ty: int) -> int:
+
+        mins = 0
+        while (tx,ty)!=(sx,sy):
+            if tx<sx or ty<sy or max(tx,ty)==0: return -1
+
+            if tx>ty:
+                hlf = tx//2
+                if hlf<<1 == tx and hlf >= ty:# current state had came by adding 'x' with 'x'
+                    tx = hlf
+                elif tx-ty<=ty and ty>0: # current state had came by adding 'x' with 'y'
+                    tx = tx-ty
+                else: return -1
+            elif tx<ty:
+                hlf = ty//2
+                if hlf<<1 == ty and hlf >= tx: # curr state had came by adding 'y' with 'y'
+                    ty = hlf
+                elif ty-tx <= tx and tx>0: # state came by adding 'y' with 'x'
+                    ty = ty-tx
+                else: return -1
+            else:
+                if sx>sy:
+                    ty=0
+                else:
+                    tx=0
+            mins+=1
+        return mins
+    
+if __name__ == '__main__':
+    tcs = []
+    add = lambda sx,sy,tx,ty: tcs.append([sx,sy,tx,ty])
+    add(sx = 1, sy = 2, tx = 5, ty = 4)
+    add(sx = 0, sy = 1, tx = 2, ty = 3)
+    add(sx = 1, sy = 1, tx = 2, ty = 2)
+    add(
+        sx =0,sy =1,tx =1000000000,ty =999999999
+    )
+
+    sol = Solution()
+    for [sx,sy,tx,ty] in tcs:
+        print(f'sx={sx} sy={sy} tx={tx} ty={ty} minMoves={sol.minMoves(sx,sy,tx,ty)}')
