d4d28dfb4ac337640d4f2c1fdec84000726acfa2
subh <harvirbuttar2917@gmail.com>
2025-07-14 19:42:39 -0600

    Tried Dp + bitmasking as starting approach to solve the problem

--- src/app/2025/July/1931.py
+++ src/app/2025/July/1931.py
@@ -27,4 +27,23 @@
 
 class Solution:
     def colorTheGrid(self, m: int, n: int) -> int:
-        pass
+        MOD, state = 10**9+7, [
+            [-1 for _ in range(1<<2*m)] for _ in range(n)
+        ]
+        def count_ways(r:int,c:int,curr:int,prev:int):
+            nonlocal MOD, state
+            if c==n: return 1
+            elif r==0 and state[c][prev]!=-1: return state[c][prev]
+            ways, up_color =  0, curr&(3<<2*(r-1))
+            for color in range(1,4):
+                left_color = prev&(3<<2*r)
+                if color not in [up_color, left_color]:
+                    diff = (r+1)//m
+                    nxt_r, nxt_c = (r+1)%m, c+diff
+                    nxt_curr, nxt_prev = [curr|(color<<(2*r+1)), 0][diff], [prev,curr][diff]
+                    ways = (
+                        ways%MOD + count_ways(nxt_r, nxt_c, nxt_curr, nxt_prev)%MOD
+                    )%MOD
+            if r==0: state[c][prev]=ways
+            return ways
+        return count_ways(0,0,0,0)
