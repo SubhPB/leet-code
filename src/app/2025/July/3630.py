bc2d835053a7b2c00b24b34168d239a7a3a16d3c
subh <harvirbuttar2917@gmail.com>
2025-07-29 18:09:57 -0600

    A quite good problem with valid structured code  but read problem incorrectly and solveed for subarrays instead solving for subsequences

---
+++ src/app/2025/July/3630.py
@@ -0,0 +1,107 @@
+'''
+    3630. Partition Array for Maximum XOR and AND
+
+    You are given an integer array nums.
+
+    Partition the array into three (possibly empty) subsequences A, B, and C such that every element of nums belongs to exactly one subsequence.
+
+    Your goal is to maximize the value of: XOR(A) + AND(B) + XOR(C)
+
+    where:
+
+    XOR(arr) denotes the bitwise XOR of all elements in arr. If arr is empty, its value is defined as 0.
+    AND(arr) denotes the bitwise AND of all elements in arr. If arr is empty, its value is defined as 0.
+    Return the maximum value achievable.
+
+    Note: If multiple partitions result in the same maximum sum, you can consider any one of them.
+
+    Example 1:
+
+    Input: nums = [2,3]
+
+    Output: 5
+
+    Explanation:
+
+    One optimal partition is:
+
+    A = [3], XOR(A) = 3
+    B = [2], AND(B) = 2
+    C = [], XOR(C) = 0
+    The maximum value of: XOR(A) + AND(B) + XOR(C) = 3 + 2 + 0 = 5. Thus, the answer is 5.
+
+    Example 2:
+
+    Input: nums = [1,3,2]
+
+    Output: 6
+
+    Explanation:
+
+    One optimal partition is:
+
+    A = [1], XOR(A) = 1
+    B = [2], AND(B) = 2
+    C = [3], XOR(C) = 3
+    The maximum value of: XOR(A) + AND(B) + XOR(C) = 1 + 2 + 3 = 6. Thus, the answer is 6.
+
+    Example 3:
+
+    Input: nums = [2,3,6,7]
+
+    Output: 15
+
+    Explanation:
+
+    One optimal partition is:
+
+    A = [7], XOR(A) = 7
+    B = [2,3], AND(B) = 2
+    C = [6], XOR(C) = 6
+    The maximum value of: XOR(A) + AND(B) + XOR(C) = 7 + 2 + 6 = 15. Thus, the answer is 15.
+
+    Constraints:
+
+    1 <= nums.length <= 19
+    1 <= nums[i] <= 10^9
+'''
+from typing import List
+class Solution:
+    def maximizeXorAndXor(self, nums: List[int]) -> int:
+
+        n = len(nums); XOR = [0]*(n+1)
+        for i in range(1,n+1): XOR[i] = nums[i-1]^XOR[i-1]
+        xor = lambda l,r: XOR[r]^XOR[l] # l-inclusive & r-exclusive
+
+        res = 0
+        for blen in range(n+1):
+            for bstart in range(n-blen+1):
+                
+                AND = nums[bstart] if blen else 0
+                for x in range(bstart+1,bstart+blen): AND&=nums[x]
+
+                if blen and bstart in [0, n-blen]: #special handle-case
+                    if blen<n:
+                        l, r = (0,bstart) if bstart>0 else (blen, n)
+                        for cstart in range(l,r):
+                            res = max(
+                                res, 
+                                xor(l,cstart) + AND + xor(cstart,r)
+                            )
+                    else: res = max(res, AND)
+                else:
+                    res = max(
+                        res, 
+                        xor(0, bstart) + AND + xor(bstart+blen, n)
+                    )
+        return res    
+            
+if __name__ == '__main__':
+    testcases = []
+    add = lambda nums:testcases.append(nums)
+    # add([2,3])
+    # add([1,3,2])
+    # add([2,3,6,7])
+    add([625,165,454,598])
+    for nums in testcases:
+        print(f'Nums={nums} result={Solution().maximizeXorAndXor(nums)}')
