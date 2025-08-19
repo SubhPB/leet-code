ebde02c3165bcd4f93baf3cdefe944470a9d38cd
subh <harvirbuttar2917@gmail.com>
2025-08-19 11:32:59 -0600

    came close to solution with more clarity

--- src/app/2025/contests/462/3646.py
+++ src/app/2025/contests/462/3646.py
@@ -28,31 +28,63 @@
     Explanation:
 
     212 is the smallest special number greater than 33, as it is a palindrome and the digits 1 and 2 appear exactly 1 and 2 times respectively.
+
+    python src/app/2025/contests/462/3646.py
 '''
 from typing import List
-from functools import cache
+from collections import deque
 
-MS=18; dp:List[List[int]] = [[] for _ in range(MS)]
+dp:List[List[int]] = [
+    [] for _ in range(sum([2,4,6,8,10]))
+]
 
-@cache
-def dfs(x):
-    dp[x].append(x)
-    tp = [x]; x = x+1 if x&1 else x+2
-    while (x<=8):
-        for csm in dfs(x):
-            if x+csm >= MS: break
-            tp.append(x+csm)
-            dp[x+csm].append(x|csm)
-        x+=2
-    return tp
+def dfs(px:int):
+    mask:List[int] = []; tn:List[int] = []
+    for x in [2,4,6,8]:
+        tn.append(px); mask.append((1<<px) if px else 0)
+        for i, sm in enumerate(tn):
+            totalSm = x+sm; newMask = mask[i] | 1<<x
+            dp[totalSm].append(newMask)
+            tn[i]=totalSm; mask[i]=newMask
 
-for x in range(1,9): dfs(x)
+dfs(0) # calculating dp for even only nums
 
-for i,x in enumerate(dp):
-    print(f'i={i}, x={[ bin(c) for c in x]}')
+for px in [3,5,7,9]:  # calculating dp evens + one odd scenario
+    dfs(px)
+    dp[px].append(1<<px) # obvious base-cases for odd nums
 
 
 class Solution:
     def specialPalindrome(self, n: int) -> int:
+        d = len(str(n)); res = float('inf')
         # Observation: Using 2 odd digits is never possible to get right answer!
-        pass
+        #    To get the minimum result we will try to find smallest candidate of same digit length as of n
+        #    If not found then we'll find smallest digit num with the digit length eql 'd+1', which is definitely an answer!
+
+        def hlfParse(x:List[int|str], m=[]):
+            return int(''.join(x + m + x.reverse()))
+        
+        
+        if not d%2:
+            # let's look for a candidate greater and having digit length as of 'd'
+            for candidate in dp[d]:
+                digits:List[int] = []
+                for i in range(candidate.bit_length()-1, -1, -1):
+                    if candidate&(1<<i): digits.append(i)
+                
+                hlf = []
+                for x in digits:
+                    for i in range(x//2): hlf.append(x)
+
+                maxPossible = hlfParse(hlf)
+                if n<maxPossible:
+                    res = min(res, maxPossible)
+                    left, right = hlf[:1], deque(hlf[1:])
+                    while right:
+                        for digit in digits:
+                                pass
+
+        else:
+            pass
+
+        return res
