e42069fe46692d9da43019f1e5c68e7f93a13c31
subh <harvirbuttar2917@gmail.com>
2025-08-29 18:27:58 -0600

    Over-solved the problem which now works correctly for L-shape, not for V-shape because of not reading problem statement properly

--- src/app/2025/Aug/3459.py
+++ src/app/2025/Aug/3459.py
@@ -67,53 +67,52 @@ class Solution:
     def lenOfVDiagonal(self, grid: List[List[int]]) -> int:
         res=0; m=len(grid); n=len(grid[0])
 
-        init = lambda: [[1]*n for _ in range(m)]
+        init = lambda: [[0]*n for _ in range(m)]
         TL,TR,BR,BL = (init() for _ in range(4))
 
-        GET = lambda x,y,T:  T[x][y] if 0<=x<m and 0<=y<n and grid[x][y] != 1 else 0
-
-
+        # TL/TR
+        for x in range(m):
+            for y in range(n):
+                if grid[x][y]!=1:
+                    TL[x][y] = 1; TR[x][y] = 1
+                    nx=x-1; ny=y-1
+                    if 0<=nx<m and 0<=ny<n and grid[nx][ny] != grid[x][y]:
+                        TL[x][y]+=TL[nx][ny]
+                    nx=x-1; ny=y+1
+                    if 0<=nx<m and 0<=ny<n and grid[nx][ny] != grid[x][y]:
+                        TR[x][y]+=TR[nx][ny]
+        # BR/BL
         for x in range(m-1,-1,-1):
             for y in range(n-1,-1,-1):
                 if grid[x][y]!=1:
-                    if GET(x+1,y+1,BR) and grid[x+1][y+1]!=grid[x][y]:
-                        BR[x][y] += GET(x+1,y+1,BR)
-                    if GET(x+1,y-1,BL) and grid[x+1][y-1] != grid[x][y]:
-                        BL[x][y] += GET(x+1,y-1,BL)
-                else:
-                    BR[x][y] = GET(x+1,y+1,BR)
-                    BL[x][y] = GET(x+1,y-1,BL)
-
-        for x in range(m):
-            for y in range(n):
-                if grid[x][y]!=1:
-                    if GET(x-1,y-1,TL) and grid[x-1][y-1] != grid[x][y]:
-                        TL[x][y] += GET(x-1,y-1,TL)
-                    if GET(x-1,y+1,TR) and grid[x-1][y+1] != grid[x][y]:
-                        TR[x][y] += GET(x-1,y+1,TR)
-                else:
-                    TL[x][y] = GET(x-1,y-1,TL)
-                    TR[x][y] = GET(x-1,y+1,TR)
-
-
+                    BR[x][y]=1; BL[x][y]=1
+                    nx=x+1; ny=y-1
+                    if 0<=nx<m and 0<=ny<n and grid[nx][ny] != grid[x][y]:
+                        BL[x][y]+=BL[nx][ny]
+                    nx=x+1; ny = y+1
+                    if 0<=nx<m and 0<=ny<n and grid[nx][ny] != grid[x][y]:
+                        BR[x][y]+=BR[nx][ny]
+        
+        TS = [TL,TR,BR,BL]; dirs = [(-1,-1),(-1,1),(1,1),(1,-1)]
         for x in range(m):
             for y in range(n):
                 if grid[x][y] == 1:
-                    delta = 0; tempRes = 1
-
-                    tl = TL[x][y]; tempRes+=tl; v = GET(x-tl,y-tl,TR)
-                    if tl: delta = max(delta, v-max(0,v-1))
-
-                    tr = TR[x][y]; tempRes+=tr; v = GET(x-tr, y+tr, BR)
-                    if tr: delta = max(delta, v-max(0,v-1))
-
-                    br = BR[x][y]; tempRes+=br; v = GET(x+br, y+br, BL)
-                    if br: delta = max(delta, v-max(0,v-1))
-
-                    bl =BL[x][y]; tempRes+=bl; v = GET(x+bl,y-bl,TL)
-                    if bl: delta = max(delta, v-max(0,v-1))
-
-                    res = max(res, tempRes+delta)
-
+                    values:List[tuple] = []
+                    for i in range(4):
+                        ix,iy = dirs[i]; T = TS[i]
+                        nx=x+ix; ny=y+iy
+                        dirMax = 0; z=0 # if this dir will sure use the  
+                        while 0<=nx<m and 0<=ny<n and T[x+iy][y+iy]>z:
+                            dirMax = max(dirMax, z+TS[(i+1)%4][nx][ny])
+                            nx+=ix; ny+=iy; z+=1
+                        values.append((z,dirMax))# (default,valueAfter90deg)
+                    
+                    defaultSums = sum([d for d,_ in values])
+                    for default, dirMax in values:
+                        res = max(
+                            res,
+                            defaultSums-default+dirMax+1
+                        )
+                        
         return res
     
