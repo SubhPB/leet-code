3dfaaf4aabdb76c4730d418ecde693e4eb5ef5c1
subh <harvirbuttar2917@gmail.com>
2025-09-03 17:55:48 -0600

    Approached #3640 but getting T.L.E at the very end large testcases

---
+++ src/app/2025/contests/465/3670.py
@@ -0,0 +1,58 @@
+'''
+3670. Maximum Product of Two Integers With No Common Bits
+    You are given an integer array nums.
+
+    Your task is to find two distinct indices i and j such that the product nums[i] * nums[j] is maximized, and the binary representations of nums[i] and nums[j] do not share any common set bits.
+
+    Return the maximum possible product of such a pair. If no such pair exists, return 0.
+
+    Example 1:
+
+    Input: nums = [1,2,3,4,5,6,7]
+
+    Output: 12
+
+    Explanation:
+
+    The best pair is 3 (011) and 4 (100). They share no set bits and 3 * 4 = 12.
+
+    Example 2:
+
+    Input: nums = [5,6,4]
+
+    Output: 0
+
+    Explanation:
+
+    Every pair of numbers has at least one common set bit. Hence, the answer is 0.
+
+    Example 3:
+
+    Input: nums = [64,8,32]
+
+    Output: 2048
+
+    Explanation:
+
+    No pair of numbers share a common bit, so the answer is the product of the two maximum elements, 64 and 32 (64 * 32 = 2048).
+
+    
+
+    Constraints:
+
+    2 <= nums.length <= 10^5
+    1 <= nums[i] <= 10^6
+'''
+from typing import List
+from collections import Counter
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res=0; hsh = Counter(nums)
+        n = len(nums); flip = lambda x: x^((1<<x.bit_length())-1)
+
+        for x in range((n+1)//2):
+            num = nums[x]; numb = flip(num)
+            while (res<numb*num and numb not in hsh) or numb&num:
+                numb-=1
+            res = max(res, numb*num)
+        return res
