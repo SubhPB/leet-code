3392613087a3b5c1fd910cf82d5e675a9b84309f
subh <harvirbuttar2917@gmail.com>
2025-09-25 16:57:27 -0600

    Bon exercice pour exercer le DP. Application du DP pour reduire la complexite spatiale

--- src/app/2025/Sept/W4.py
+++ src/app/2025/Sept/W4.py
@@ -1,4 +1,4 @@
-
+from typing import List
 
 class Solution:
     '''
@@ -56,3 +56,29 @@ class Solution:
             if x>y: return 1
             elif y>x: return -1
         return 0
+    ''' Q120
+    Given a triangle array, return the minimum path sum from top to bottom.
+    For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+
+    Example 1:
+
+    Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+    Output: 11
+    Explanation: The triangle looks like:
+    2
+    3 4
+    6 5 7
+    4 1 8 3
+    The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
+    Example 2:
+
+    Input: triangle = [[-10]]
+    Output: -10
+    '''
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        # Main challenge was to solve in O(n) space complexity!
+        dp = list(triangle[-1]); n = len(triangle)
+        for row in range(n-2,-1,-1):
+            for col in range(row+1):
+                dp[col] = triangle[row][col] + min(dp[col], dp[col+1])
+        return dp[0]
