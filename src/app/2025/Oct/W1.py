08d22cc58063e2217beb7bd17204ba9592af35eb
subh <harvirbuttar2917@gmail.com>
2025-10-03 13:11:54 -0600

    un problème difficile mais très bon pour partiquer la capacité à créer des solutions simples de systèmes complexes. cette solution n'est pas correcte mais très bon comme première tentative. ll reste juste à gérer quelque cus limites, ce que je ferai demain.

--- src/app/2025/Oct/W1.py
+++ src/app/2025/Oct/W1.py
@@ -1,3 +1,6 @@
+from collections import deque
+from typing import List
+
 class Solution:
     '''
     You are given two integers numBottles and numExchange.
@@ -14,3 +17,69 @@ class Solution:
             numBottles-=numExchange
             numBottles+=1;numExchange+=1;res+=1
         return res
+    '''
+    407. Trapping Rain Water II
+
+    Given an m x n integer matrix heightMap representing the height of each unit cell in a 2D elevation map, return the volume of water it can trap after raining.
+
+    Example 1:
+
+    Input: heightMap = [[1,4,3,1,3,2],[3,2,1,3,2,4],[2,3,3,2,3,1]]
+    Output: 4
+    Explanation: After the rain, water is trapped between the blocks.
+    We have two small ponds 1 and 3 units trapped.
+    The total volume of water trapped is 4.
+    
+    Constraints:
+        m == heightMap.length
+        n == heightMap[i].length
+        1 <= m, n <= 200
+        0 <= heightMap[i][j] <= 2 * 10^4
+    '''
+    def trapRainWater(self, heightMap: List[List[int]]) -> int:
+        maxHeight=0; ans=0
+        m=len(heightMap); n=len(heightMap[0])
+
+        for heights in heightMap:
+            maxHeight = max(maxHeight, max(heights))
+
+        # dummy value
+        heightMap[m-1].append(0)
+        
+        # prevMinHeight = 2 * 10**4 # may use later
+
+        def neighbors(x:int,y:int):
+            N = []
+            for i,j in [(0,1),(0,-1),(1,0),(-1,0)]:
+                r = x+i; c = y+j
+                if 0<=r<m and 0<=c<n: 
+                    N.append((r,c))
+            if len(N) != 4: # dummy value
+                N.append((m-1,n))
+            return N
+
+        for height in range(maxHeight+1):
+            #Guarantee: minHeight among heightMap >= height
+            for r in range(m):
+                for c in range(n):
+                    if height == heightMap[r][c]:
+                        q = deque([(r,c)]); rq = []
+                        traversed = set()
+                        threshold = 2 * 10**4
+                        while len(q):
+                            rq.append(q.popleft())
+                            currX,currY = rq[-1]
+                            for x,y in neighbors(currX,currY):
+                                if (x,y) in traversed: continue
+                                ht = heightMap[x][y]
+                                if ht == height and (x,y) != (m-1,n):
+                                    q.append((x,y))
+                                else: 
+                                    threshold = min(threshold,ht)
+                            traversed.add((currX,currY))
+                        for nx,ny in rq:
+                            ans += max(0, threshold-height)
+                            heightMap[nx][ny] = threshold
+                        
+        return ans
+
