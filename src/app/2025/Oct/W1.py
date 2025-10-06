b58ca91ba3c59fae0af743e95b391b0728371f24
SubhPB <harvirbuttar2917@gmail.com>
2025-10-06 15:20:21 -0700

    écrit dur mais en fait un problème facile. tout a été résolu uniquement par l'algorithme de Dijkstra

--- src/app/2025/Oct/W1.py
+++ src/app/2025/Oct/W1.py
@@ -1,4 +1,5 @@
 from collections import deque
+import heapq
 from typing import List
 
 class Solution:
@@ -148,3 +149,52 @@ class Solution:
                 if (row,col) in pacific and (row,col) in altantic:
                     res.append([row,col])
         return res
+    '''
+    778. Swim in Rising Water
+
+    You are given an n x n integer matrix grid where each value grid[i][j] represents the elevation at that point (i, j).
+    It starts raining, and water gradually rises over time. At time t, the water level is t, meaning any cell with elevation less than equal to t is submerged or reachable.
+    You can swim from a square to another 4-directionally adjacent square if and only if the elevation of both squares individually are at most t. You can swim infinite distances in zero time. Of course, you must stay within the boundaries of the grid during your swim.
+    Return the minimum time until you can reach the bottom right square (n - 1, n - 1) if you start at the top left square (0, 0). 
+
+    Example 1:
+
+    Input: grid = [[0,2],[1,3]]
+    Output: 3
+    Explanation:
+    At time 0, you are in grid location (0, 0).
+    You cannot go anywhere else because 4-directionally adjacent neighbors have a higher elevation than t = 0.
+    You cannot reach point (1, 1) until time 3.
+    When the depth of water is 3, we can swim anywhere inside the grid.
+    Example 2:
+
+
+    Input: grid = [[0,1,2,3,4],[24,23,22,21,5],[12,13,14,15,16],[11,17,18,19,20],[10,9,8,7,6]]
+    Output: 16
+    Explanation: The final route is shown.
+    We need to wait until time 16 so that (0, 0) and (4, 4) are connected.
+
+    Constraints:
+
+    n == grid.length
+    n == grid[i].length
+    1 <= n <= 50
+    0 <= grid[i][j] < n2
+    Each value grid[i][j] is unique.
+    '''
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n=len(grid); minDist=[[n**2+1]*n for _ in range(n)]
+        h = [(grid[0][0],0,0)] # (dist,x,y)
+        while h:
+            d,x,y=heapq.heappop(h)
+            if x==y==n-1: 
+                return d
+            elif d<minDist[x][y]:
+                minDist[x][y]=d
+                for i,j in [(0,1),(0,-1),(1,0),(-1,0)]:
+                    nr=x+i;ny=y+j
+                    if 0<=nr<n and 0<=ny<n:
+                        nt=grid[nr][ny]; dt=max(0,nt-d)
+                        heapq.heappush(h,(d+dt,nr,ny))
+            
+        return -1
