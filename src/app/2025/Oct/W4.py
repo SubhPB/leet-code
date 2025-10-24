31b9301bd4c4ebe7c57a211fd2fe3fc810061a3a
SubhPB <harvirbuttar2917@gmail.com>
2025-10-23 18:13:34 -0700

    je n'ai actuellement aucune ideé de ce qu'il faut faire en cas de problème

--- src/app/2025/Oct/W4.py
+++ src/app/2025/Oct/W4.py
@@ -79,3 +79,35 @@ class Solution:
             for i in range(1,len(x)): nx.append((x[i-1]+x[i])%10)
             x=nx
         return x[0]==x[1]
+    def c():
+        '''
+  
+        An integer x is numerically balanced if for every digit d in the number x, there are exactly d occurrences of that digit in x.
+
+        Given an integer n, return the smallest numerically balanced number strictly greater than n.
+
+        Example 1:
+
+        Input: n = 1
+        Output: 22
+        Explanation: 
+        22 is numerically balanced since:
+        - The digit 2 occurs 2 times. 
+        It is also the smallest numerically balanced number strictly greater than 1.
+        Example 2:
+
+        Input: n = 1000
+        Output: 1333
+        Explanation: 
+        1333 is numerically balanced since:
+        - The digit 1 occurs 1 time.
+        - The digit 3 occurs 3 times. 
+        It is also the smallest numerically balanced number strictly greater than 1000.
+        Note that 1022 cannot be the answer because 0 appeared more than 0 times.
+        Example 3:
+
+
+        Constraints:
+
+        0 <= n <= 10^6
+        '''
