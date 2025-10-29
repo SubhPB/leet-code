7428a8a2ed1b4b6e4423c1fb45e952ace2ead4ab
subh <harvirbuttar2917@gmail.com>
2025-10-28 17:54:52 -0700

    voilà un problème stimulant qui semble intéressant. je verrai plus tard quelle approche intuitive permettra de le résoudre.

--- src/app/2025/contests/C168.py
+++ src/app/2025/contests/C168.py
@@ -14,16 +14,11 @@ class Solution:
 
         Return a string denoting the good integer n that achieves the maximum score. If there are multiple possible integers, return the maximum ​​​​​​​one. If no such integer exists, return an empty string.
 
-        
-
         Example 1:
-
         Input: num = 2, sum = 3
-
         Output: "30"
 
         Explanation:
-
         There are 3 good integers: 12, 21, and 30.
 
         The score of 12 is 12 + 22 = 5.
@@ -32,33 +27,16 @@ class Solution:
         The maximum score is 9, which is achieved by the good integer 30. Therefore, the answer is "30".
 
         Example 2:
-
         Input: num = 2, sum = 17
-
         Output: "98"
 
         Explanation:
-
         There are 2 good integers: 89 and 98.
-
         The score of 89 is 82 + 92 = 145.
         The score of 98 is 92 + 82 = 145.
         The maximum score is 145. The maximum good integer that achieves this score is 98. Therefore, the answer is "98".
 
-        Example 3:
-
-        Input: num = 1, sum = 10
-
-        Output: ""
-
-        Explanation:
-
-        There are no integers that have exactly 1 digit and whose digits sum to 10. Therefore, the answer is "".
-
-        
-
         Constraints:
-
         1 <= num <= 2 * 10^5
         1 <= sum <= 2 * 10^6
     '''
@@ -71,5 +49,36 @@ class Solution:
             for i in range(q): res.append(str(d))
         
         return ''.join(res)+''.join(["0"]*max(0,num-len(res)))
+    '''
+    3724. Minimum Operations to Transform Array
 
+    You are given two integer arrays nums1 of length n and nums2 of length n + 1.
+    You want to transform nums1 into nums2 using the minimum number of operations.
+    You may perform the following operations any number of times, each time choosing an index i:
+
+    Increase nums1[i] by 1.
+    Decrease nums1[i] by 1.
+    Append nums1[i] to the end of the array.
+    Return the minimum number of operations required to transform nums1 into nums2.
+
+    Example 1:
+
+    Input: nums1 = [2,8], nums2 = [1,7,3]
+    Output: 4
+    Explanation:
+    Step	i	Operation	nums1[i]	Updated nums1
+    1	0	Append	-	[2, 8, 2]
+    2	0	Decrement	Decreases to 1	[1, 8, 2]
+    3	1	Decrement	Decreases to 7	[1, 7, 2]
+    4	2	Increment	Increases to 3	[1, 7, 3]
+    Thus, after 4 operations nums1 is transformed into nums2.
+    
+    Constraints:
+
+        1 <= n == nums1.length <= 10^5
+        nums2.length == n + 1
+        1 <= nums1[i], nums2[i] <= 10^5
+    '''
+    def minOperations(self, nums1: List[int], nums2: List[int]) -> int:
+        pass
         
