5a5fc049fd28259a6abafb5486d23ca43b26f134
subh <harvirbuttar2917@gmail.com>
2025-11-13 11:53:47 -0800

    une stratégie de solution intuitive plutôt efficace, mais qui échoue au niveau #958 en raison de sa complexité temporelle.

--- src/app/2025/Nov/W2.py
+++ src/app/2025/Nov/W2.py
@@ -48,3 +48,46 @@ class Solution:
                         1+dp[i-y][j-x]
                     )
         return dp[m][n]
+    '''
+    3542. Minimum Operations to Convert All Elements to Zero
+    You are given an array nums of size n, consisting of non-negative integers. Your task is to apply some (possibly zero) operations on the array so that all elements become 0.
+    In one operation, you can select a subarray [i, j] (where 0 <= i <= j < n) and set all occurrences of the minimum non-negative integer in that subarray to 0.
+    Return the minimum number of operations required to make all elements in the array 0.
+
+    Example 1:
+    Input: nums = [0,2]
+    Output: 1
+    Explanation:
+    Select the subarray [1,1] (which is [2]), where the minimum non-negative integer is 2. Setting all occurrences of 2 to 0 results in [0,0].
+    Thus, the minimum number of operations required is 1.
+    Example 2:
+    Input: nums = [3,1,2,1]
+    Output: 3
+
+    Explanation:
+
+    Select subarray [1,3] (which is [1,2,1]), where the minimum non-negative integer is 1. Setting all occurrences of 1 to 0 results in [3,0,2,0].
+    Select subarray [2,2] (which is [2]), where the minimum non-negative integer is 2. Setting all occurrences of 2 to 0 results in [3,0,0,0].
+    Select subarray [0,0] (which is [3]), where the minimum non-negative integer is 3. Setting all occurrences of 3 to 0 results in [0,0,0,0].
+    Thus, the minimum number of operations required is 3.
+
+    Constraints:
+    1 <= n == nums.length <= 10^5
+    0 <= nums[i] <= 10^5
+    '''
+    def minOperations(self, nums: List[int]) -> int:
+        n=len(nums);freq={};res=0
+        for i in range(n):
+            if nums[i] not in freq:
+                freq[nums[i]]=[]
+            freq[nums[i]].append(i)
+        for idxs in freq.values():
+            m=len(idxs); num=nums[idxs[0]]
+            if not num:continue
+            res+=1
+            for i in range(m-1):
+                for j in range(idxs[i],idxs[i+1]):
+                    if nums[j]<num:
+                        res+=1;i+=1
+                        break
+        return res
