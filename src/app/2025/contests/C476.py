a8fbecf236859a144ea22524e2133c2586aaf0f2
subh <harvirbuttar2917@gmail.com>
2025-11-17 17:36:35 -0800

    le problème #3747 était un excellent défi qui m'a permis de pratiquer la prgrammation et les mathématiques.

--- src/app/2025/contests/C476.py
+++ src/app/2025/contests/C476.py
@@ -28,3 +28,28 @@ class Solution:
     def minLengthAfterRemovals(self, s: str) -> int:
         a=s.count('a');b=len(s)-a
         return abs(a-b)
+    '''
+    3747. Count Distinct Integers After Removing Zeros
+    You are given a positive integer n.
+    For every integer x from 1 to n, we write down the integer obtained by removing all zeros from the decimal representation of x.
+    Return an integer denoting the number of distinct integers written down.
+
+    Example 1:
+    Input: n = 10
+    Output: 9
+    Explanation:
+    The integers we wrote down are 1, 2, 3, 4, 5, 6, 7, 8, 9, 1. There are 9 distinct integers (1, 2, 3, 4, 5, 6, 7, 8, 9).
+
+    Constraints:
+    1 <= n <= 10^15
+    '''
+    def countDistinct(self, n: int) -> int:
+        digits=[int(dgt) for dgt in str(n)]
+        l=len(digits); fn=lambda N:-1+((9**(N)-1)//(9-1))
+        i=0;res=fn(l)+1 # inc by 1 because n is itself unique if not include zero (assumed it not has zero)
+        while i<l:
+            if not digits[i]: 
+                res-=1; break # it had zero, pre-assumption was false! 
+            res+=(digits[i]-1)*(9**(l-i-1))
+            i+=1
+        return res
