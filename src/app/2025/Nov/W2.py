41eee7039c5ffb0caeaeab8b50acb17ddc80aa38
subh <harvirbuttar2917@gmail.com>
2025-11-21 11:31:31 -0800

    #757 ce problème m'a convaincu que tout problème complexe peut être résolu par des étapes intuitives simples mais profondes.

--- src/app/2025/Nov/W2.py
+++ src/app/2025/Nov/W2.py
@@ -82,3 +82,33 @@ class Solution:
             if not st or st[-1]<num: 
                 st.append(num);res+=min(1,num)
         return res
+    '''
+    757. Set Intersection Size At Least Two
+
+    You are given a 2D integer array intervals where intervals[i] = [starti, endi] represents all the integers from starti to endi inclusively.
+    A containing set is an array nums where each interval from intervals has at least two integers in nums.
+    For example, if intervals = [[1,3], [3,7], [8,9]], then [1,2,4,7,8,9] and [2,3,4,8,9] are containing sets.
+    Return the minimum possible size of a containing set.
+
+    Example 1:
+
+    Input: intervals = [[1,3],[3,7],[8,9]]
+    Output: 5
+    Explanation: let nums = [2, 3, 4, 8, 9].
+    It can be shown that there cannot be any containing array of size 4.
+    Constraints:
+
+    1 <= intervals.length <= 3000
+    intervals[i].length == 2
+    0 <= starti < endi <= 10^8
+    '''
+    def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
+        res=0;p1=-1;p2=-1;intervals.sort(key=lambda inv:(inv[1],-inv[0]))
+        for [l,r] in intervals:
+            if p2<l:
+                p1,p2=r-1,r
+                res+=2
+            elif p1<l:
+                res+=1
+                p1,p2=p2,r
+        return res
