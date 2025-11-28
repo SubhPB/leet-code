b30ae49aaf9fb256934e058d8aaf8c77b84f8cb1
subh <harvirbuttar2917@gmail.com>
2025-11-28 10:37:16 -0800

    ce n'est pas correct pour l'instant car j'ai mal interprété l'énoncé du probléme, mais la méthode par force brute que j'appliqureai demain devrait fonctionner sans probléme.

--- src/app/2025/Nov/W3.py
+++ src/app/2025/Nov/W3.py
@@ -40,3 +40,42 @@ class Solution:
                         else: 
                             dp[r][c][e]=dp[r+1][c][en]
         return dp[0][0][0]
+    '''
+    3751. Total Waviness of Numbers in Range I
+
+    You are given two integers num1 and num2 representing an inclusive range [num1, num2].
+    The waviness of a number is defined as the total count of its peaks and valleys:
+    A digit is a peak if it is strictly greater than both of its immediate neighbors.
+    A digit is a valley if it is strictly less than both of its immediate neighbors.
+    The first and last digits of a number cannot be peaks or valleys.
+    Any number with fewer than 3 digits has a waviness of 0.
+    Return the total sum of waviness for all numbers in the range [num1, num2].
+
+    Example 1:
+    Input: num1 = 120, num2 = 130
+    Output: 3
+
+    Explanation:
+    In the range [120, 130]:
+    120: middle digit 2 is a peak, waviness = 1.
+    121: middle digit 2 is a peak, waviness = 1.
+    130: middle digit 3 is a peak, waviness = 1.
+    All other numbers in the range have a waviness of 0.
+    Thus, total waviness is 1 + 1 + 1 = 3.
+
+    Constraints:
+    1 <= num1 <= num2 <= 10^5
+    '''
+    def totalWaviness(self, num1: int, num2: int) -> int:
+        E=0
+        def eva(s:str,i:int):
+            n=len(s)
+            if n>2:
+                arr=sorted([int(s[j]) for j in range(i-1,i+2)])
+                return int(s[i]!=str(arr[1]))
+            return 0
+        for num in range(num1,num2+1):
+            num=str(num);n=len(num)
+            E+=eva(num,n//2)
+            if not n%2: E+=eva(num,n//2-1)
+        return E
