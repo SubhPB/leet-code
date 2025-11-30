d794da46afd4a12e3c9cde50894b92121795e750
subh <harvirbuttar2917@gmail.com>
2025-11-30 14:30:59 -0800

    Aujourd'hui, je ne derai pas que je suis trop bête pour résoudre ce problème mais je n'ai pas encore suffisamment étudié la question pour y répondre

--- src/app/2025/Nov/W3.py
+++ src/app/2025/Nov/W3.py
@@ -74,3 +74,53 @@ class Solution:
                 arr=sorted([num[i+j] for j in (-1,0,1)],key=lambda x:int(x))
                 E+=int(arr[1]!=num[i])
         return E
+    '''
+    3752. Lexicographically Smallest Negated Permutation that Sums to Target
+    You are given a positive integer n and an integer target.
+    Return the lexicographically smallest array of integers of size n such that:
+
+    The sum of its elements equals target.
+    The absolute values of its elements form a permutation of size n.
+    If no such array exists, return an empty array.
+
+    A permutation of size n is a rearrangement of integers 1, 2, ..., n.
+
+    Example 1:
+
+    Input: n = 3, target = 0
+    Output: [-3,1,2]
+
+    Explanation:
+    The arrays that sum to 0 and whose absolute values form a permutation of size 3 are:
+
+    [-3, 1, 2]
+    [-3, 2, 1]
+    [-2, -1, 3]
+    [-2, 3, -1]
+    [-1, -2, 3]
+    [-1, 3, -2]
+    [1, -3, 2]
+    [1, 2, -3]
+    [2, -3, 1]
+    [2, 1, -3]
+    [3, -2, -1]
+    [3, -1, -2]
+    The lexicographically smallest one is [-3, 1, 2].
+
+    Constraints:
+
+    1 <= n <= 10^5
+    -1010 <= target <= 10^10
+    '''
+    def lexSmallestNegatedPerm(self, n: int, target: int) -> List[int]:
+        '''
+        Esum=target & abs(output[i]) ~ permutation 
+        '''
+        y=(n*(n+1))//2;nums=[x for x in range(n,0,-1)]
+        if -target<=y<=target: 
+            for i in range(n):
+                yi=y-2*nums[i]
+                # if yi> will think again about the algo!
+            nums.sort()
+            return nums
+        return []
