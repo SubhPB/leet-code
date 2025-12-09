5a52259a29c7f7cfbe4a726e19d53592130fb9b4
subh <harvirbuttar2917@gmail.com>
2025-12-09 11:20:52 -0800

    je sais déjà comment prcéder approche #3757 mais je le ferai la prochaine fois, pas aujourd'hui en raison d'autres obligations professionnelles.

--- src/app/2025/contests/C477.py
+++ src/app/2025/contests/C477.py
@@ -24,5 +24,32 @@ class Solution:
             if (xor,df) in hash: res=max(res,i-hash[(xor,df)])
             else: hash[(xor,df)]=i
         return res
+    '''
+    3757. Number of Effective Subsequences
+    You are given an integer array nums.
+    The strength of the array is defined as the bitwise OR of all its elements.
+    A subsequence is considered effective if removing that subsequence strictly decreases the strength of the remaining elements.
+    Return the number of effective subsequences in nums. Since the answer may be large, return it modulo 109 + 7.
+    The bitwise OR of an empty array is 0.
 
-        
+    Example 1:
+    Input: nums = [1,2,3]
+    Output: 3
+    Explanation:
+    The Bitwise OR of the array is 1 OR 2 OR 3 = 3.
+    Subsequences that are effective are:
+    [1, 3]: The remaining element [2] has a Bitwise OR of 2.
+    [2, 3]: The remaining element [1] has a Bitwise OR of 1.
+    [1, 2, 3]: The remaining elements [] have a Bitwise OR of 0.
+    Thus, the total number of effective subsequences is 3.
+
+    Constraints:
+    1 <= nums.length <= 10^5
+    1 <= nums[i] <= 10^6
+    '''
+    def countEffective(self, nums: List[int]) -> int:
+        '''
+            Intuition basis W #subsequences = 2^n - 1 
+            <--G1-->,num,<--G2-->,num,...,num,<--Gn-->
+        '''
+        pass
