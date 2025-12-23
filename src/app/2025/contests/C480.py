cccb87dc786825a7b660e5e4b0a45a989d190348
subh <harvirbuttar2917@gmail.com>
2025-12-23 10:53:34 -0800

    #3777 le problème semble intéressant, j'y reviendrai dans un moment.

--- src/app/2025/contests/C480.py
+++ src/app/2025/contests/C480.py
@@ -39,3 +39,36 @@ class Solution:
                 temp=min(-balance[x],balance[r%n])
                 balance[x]+=temp; m+=temp*rs; r+=1
         return m
+    '''
+    3777. Minimum Deletions to Make Alternating Substring
+
+    You are given a string s of length n consisting only of the characters 'A' and 'B'.
+    You are also given a 2D integer array queries of length q, where each queries[i] is one of the following:
+    [1, j]: Flip the character at index j of s i.e. 'A' changes to 'B' (and vice versa). This operation mutates s and affects subsequent queries.
+    [2, l, r]: Compute the minimum number of character deletions required to make the substring s[l..r] alternating. This operation does not modify s; the length of s remains n.
+    A substring is alternating if no two adjacent characters are equal. A substring of length 1 is always alternating.
+
+    Return an integer array answer, where answer[i] is the result of the ith query of type [2, l, r].
+
+    Example 1:
+    Input: s = "ABA", queries = [[2,1,2],[1,1],[2,0,2]]
+    Output: [0,2]
+    Explanation:
+    i	queries[i]	j	l	r	s before query	s[l..r]	Result	Answer
+    0	[2, 1, 2]	-	1	2	"ABA"	"BA"	Already alternating	0
+    1	[1, 1]	1	-	-	"ABA"	-	Flip s[1] from 'B' to 'A'	-
+    2	[2, 0, 2]	-	0	2	"AAA"	"AAA"	Delete any two 'A's to get "A"	2
+    Thus, the answer is [0, 2].
+
+    Constraints:
+    1 <= n == s.length <= 10^5
+    s[i] is either 'A' or 'B'.
+    1 <= q == queries.length <= 10^5
+    queries[i].length == 2 or 3
+    queries[i] == [1, j] or,
+    queries[i] == [2, l, r]
+    0 <= j <= n - 1
+    0 <= l <= r <= n - 1
+    '''
+    def minDeletions(self, s: str, queries: List[List[int]]) -> List[int]:
+        pass
