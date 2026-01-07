1f2d68747f310db16dc7ad5b08d31574ee651c79
subh <harvirbuttar2917@gmail.com>
2026-01-07 10:07:03 -0800

    #3796: ce problème semble avoir un algorithme simple, mais l'implémentation du code de manière fluide s'avère un peu complexe.

--- src/app/2026/contests/C173.py
+++ src/app/2026/contests/C173.py
@@ -33,3 +33,29 @@ class Solution:
                     s[nums[j]]+=1
                 j+=1
         return res if res<=n else -1
+    '''
+    3796. Find Maximum Value in a Constrained Sequence
+    You are given an integer n, a 2D integer array restrictions, and an integer array diff of length n - 1.
+    Your task is to construct a sequence of length n, denoted by a[0], a[1], ..., a[n - 1], such that it satisfies the following conditions:
+    a[0] is 0.All elements in the sequence are non-negative.
+    For every index i (0 <= i <= n - 2), abs(a[i] - a[i + 1]) <= diff[i].
+    For each restrictions[i] = [idx, maxVal], the value at position idx in the sequence must not exceed maxVal (i.e., a[idx] <= maxVal).
+    Your goal is to construct a valid sequence that maximizes the largest value within the sequence while satisfying all the above conditions.
+    Return an integer denoting the largest value present in such an optimal sequence.
+
+    Example 1:
+    Input: n = 10, restrictions = [[3,1],[8,1]], diff = [2,2,3,1,4,5,1,1,2]
+    Output: 6
+    Explanation:
+    The sequence a = [0, 2, 4, 1, 2, 6, 2, 1, 1, 3] satisfies the given constraints (a[3] <= 1 and a[8] <= 1).
+    The maximum value in the sequence is 6.
+
+    Constraints:
+    2 <= n <= 10^5; 1 <= restrictions.length <= n - 1
+    restrictions[i].length == 2; restrictions[i] = [idx, maxVal]
+    1 <= idx < n; 1 <= maxVal <= 10^6
+    diff.length == n - 1; 1 <= diff[i] <= 10
+    The values of restrictions[i][0] are unique.
+    '''
+    def findMaxVal(self, n: int, restrictions: List[List[int]], diff: List[int]) -> int:
+        pass
