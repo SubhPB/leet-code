92e35901c93dd1e0521d3f68a24050bd1e1085d1
subh <harvirbuttar2917@gmail.com>
2026-01-12 11:23:17 -0800

    #3800: C'était une tentative plutôt réussie, mais elle nécessiterait probablement quelques retouches aprés un peu de repos.

--- src/app/2026/contests/C483.py
+++ src/app/2026/contests/C483.py
@@ -32,3 +32,45 @@ class Solution:
                         if len(set((t,l,b,r)))!=4 or w[b][3]!=w[r][3] or w[t][3]!=w[r][0]: continue
                         res.append([w[x] for x in [t,l,r,b]])
         return sorted(res)
+    '''
+    3800. Minimum Cost to Make Two Binary Strings Equal
+    You are given two binary strings s and t, both of length n, and three positive integers flipCost, swapCost, and crossCost.
+    You are allowed to apply the following operations any number of times (in any order) to the strings s and t:
+    Choose any index i and flip s[i] or t[i] (change '0' to '1' or '1' to '0'). The cost of this operation is flipCost.
+    Choose two distinct indices i and j, and swap either s[i] and s[j] or t[i] and t[j]. The cost of this operation is swapCost.
+    Choose an index i and swap s[i] with t[i]. The cost of this operation is crossCost.
+    Return an integer denoting the minimum total cost needed to make the strings s and t equal.
+
+    Example 1:
+    Input: s = "01000", t = "10111", flipCost = 10, swapCost = 2, crossCost = 2
+    Output: 16
+    Explanation:
+    We can perform the following operations:
+    Swap s[0] and s[1] (swapCost = 2). After this operation, s = "10000" and t = "10111".
+    Cross swap s[2] and t[2] (crossCost = 2). After this operation, s = "10100" and t = "10011".
+    Swap s[2] and s[3] (swapCost = 2). After this operation, s = "10010" and t = "10011".
+    Flip s[4] (flipCost = 10). After this operation, s = t = "10011".
+    The total cost is 2 + 2 + 2 + 10 = 16.
+
+    Constraints:
+    n == s.length == t.length
+    1 <= n <= 10^5​​​​​​​
+    1 <= flipCost, swapCost, crossCost <= 10^9
+    s and t consist only of the characters '0' and '1'.
+  
+    '''
+    def minimumCost(self, s: str, t: str, flipCost: int, swapCost: int, crossCost: int) -> int:
+        n=len(s); m=0; x=0
+        for i in range(n): 
+            if s[i]!=t[i]: 
+                m+=1; x+=int(s[i])
+        if 2*flipCost<=min(swapCost,crossCost): return m*flipCost
+        cost=0; inf=10**9+7
+        while x>0:
+           sc = swapCost if m-x-1>0 else inf
+           cc = crossCost if x-1>0 else inf
+           cost+=min(sc,cc)
+           if sc<cc: x-=1
+           else: x-=2
+           m-=2
+        return cost+m*flipCost
