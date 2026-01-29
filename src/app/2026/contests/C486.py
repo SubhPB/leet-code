0baa3ac306f0e7417eccbe822e70db2014d0b3f7
subh <harvirbuttar2917@gmail.com>
2026-01-29 11:24:25 -0800

    L'intiative semble reposer sur un problème de combinatoire.

--- src/app/2026/contests/C486.py
+++ src/app/2026/contests/C486.py
@@ -84,3 +84,27 @@ class Solution:
             [a,b,c]=sorted([dist[i] for dist in (dx,dy,dz)])
             res+=int(c*c==a*a+b*b)
         return res
+    '''
+    3821. Find Nth Smallest Integer With K One Bits
+    You are given two positive integers n and k.
+    Return an integer denoting the nth smallest positive integer that has exactly k ones in its binary representation. It is guaranteed that the answer is strictly less than 250.
+
+    Example 1:
+    Input: n = 4, k = 2
+    Output: 9
+    Explanation:
+    The 4 smallest positive integers that have exactly k = 2 ones in their binary representations are:
+
+    3 = 112
+    5 = 1012
+    6 = 1102
+    9 = 10012
+    
+    Constraints:
+
+    1 <= n <= 2^50
+    1 <= k <= 50
+    The answer is strictly less than 2^50
+    '''
+    def nthSmallest(self, n: int, k: int) -> int:
+        pass
