2fc4026c03c45d56e97a0a9034fd416491487675
subh <harvirbuttar2917@gmail.com>
2026-03-07 13:45:41 -0800

    #3854: une tentative louable, mais malheursement je n'ai pas lu la description en entier; le code ne fonctionne que pour l'addition, mais je le mettrai

--- src/app/2026/contests/BWC177.ts
+++ src/app/2026/contests/BWC177.ts
@@ -38,4 +38,50 @@ class BiweeklyContest177 {
         };
         return res.join('');
     };
+    /**
+     * 3854. Minimum Operations to Make Array Parity Alternating
+
+    You are given an integer array nums.
+    An array is called parity alternating if for every index i where 0 <= i < n - 1, nums[i] and nums[i + 1] have different parity (one is even and the other is odd).
+    In one operation, you may choose any index i and either increase nums[i] by 1 or decrease nums[i] by 1.
+    Return an integer array answer of length 2 where:
+    answer[0] is the minimum number of operations required to make the array parity alternating.
+    answer[1] is the minimum possible value of max(nums) - min(nums) taken over all arrays that are parity alternating and can be obtained by performing exactly answer[0] operations.
+    An array of length 1 is considered parity alternating.
+    
+    Example 1:
+    Input: nums = [-2,-3,1,4]
+    Output: [2,6]
+    Explanation:
+    Applying the following operations:
+    Increase nums[2] by 1, resulting in nums = [-2, -3, 2, 4].
+    Decrease nums[3] by 1, resulting in nums = [-2, -3, 2, 3].
+    The resulting array is parity alternating, and the value of max(nums) - min(nums) = 3 - (-3) = 6 is the minimum possible among all parity alternating arrays obtainable using exactly 2 operations.
+
+    Constraints:
+    1 <= nums.length <= 10^5
+    -10^9 <= nums[i] <= 10^9
+    */
+    makeParityAlternating(nums: number[]): number[] {
+        const inf=10**9+1, n=nums.length;
+        let ε=0, θ=0, εx=-inf;
+        let εn=inf, θx=-inf, θn=inf;
+        /** Catch: one operation can either add or subtract.
+         * But this works for addition only.
+         */
+        for(let i=0; i<n; i++){
+            if (nums[i]%2){
+                ε+=(i+1)%2; θ+=i%2;
+            } else {
+                θ+=(i+1)%2; ε+=i%2
+            };
+            εx=Math.max(εx,nums[i] + nums[i]%2 ? (i+1)%2 : i%2); 
+            εn=Math.min(εn,nums[i] + nums[i]%2 ? (i+1)%2 : i%2);
+            θx=Math.max(θx,nums[i] + nums[i]%2 ? i%2 : (i+1)%2);
+            θn=Math.min(θn,nums[i] + nums[i]%2 ? i%2 : (i+1)%2);
+        };
+        if (ε<θ) return [ε,εx-εn] 
+        else if (ε>θ) return [θ,θx-θn] 
+        return [ε,Math.min(εx-εn,θx-θn)] 
+    };
 }
