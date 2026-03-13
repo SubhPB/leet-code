7073e28e69485d7bd5f3507f6d89436a8308433f
subh <harvirbuttar2917@gmail.com>
2026-03-13 09:56:01 -0700

    #3858: j'ai créé une stratégie dfe résolution globale, qui implique le masquage de bits et une stratégie de résolution astcieuse.

--- src/app/2026/contests/C491.ts
+++ src/app/2026/contests/C491.ts
@@ -32,4 +32,34 @@ class Contest491 { //#3857-59
     minCost(n: number): number { //O(1)
         return this.dp[n]
     };
+
+    /**
+     * 3858. Minimum Bitwise OR From Grid
+        You are given a 2D integer array grid of size m x n.
+        You must select exactly one integer from each row of the grid.
+        Return an integer denoting the minimum possible bitwise OR of the selected integers from each row.
+
+        Example 1:
+        Input: grid = [[1,5],[2,4]]
+        Output: 3
+        Explanation:
+        Choose 1 from the first row and 2 from the second row.
+        The bitwise OR of 1 | 2 = 3​​​​​​​, which is the minimum possible.
+        
+        Constraints:
+        1 <= m == grid.length <= 10^5
+        1 <= n == grid[i].length <= 10^5
+        m * n <= 10^5
+        1 <= grid[i][j] <= 10^5
+    */
+    minimumOR(grid: number[][]): number {
+        /**
+         * Rough Idea:
+         * max_bit_length_possible : x = 1 + log2(10**5)//1
+         * mask = 1<<(x)-1
+         * for each bit starting left to right, for each row check if atleast 1x bit fits inside the temp mask,
+         * if any row it fails then conclude that we can't remove the bit else it is removable
+         */
+        return -1;  
+    };
 }
