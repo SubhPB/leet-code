046337dbd0eebebd5571c43fca5ef155fc516a74
subh <harvirbuttar2917@gmail.com>
2026-03-14 10:49:56 -0700

    #3859: initialisé, je réglerai le problème demain

--- src/app/2026/contests/C491.ts
+++ src/app/2026/contests/C491.ts
@@ -62,4 +62,31 @@ class Contest491 { //#3857-59
          */
         return -1;  
     };
+    /**
+     * 3859. Count Subarrays With K Distinct Integers
+    You are given an integer array nums and two integers k and m.
+    Return an integer denoting the count of subarrays of nums such that:
+
+    The subarray contains exactly k distinct integers.
+    Within the subarray, each distinct integer appears at least m times.
+    
+    Example 1:
+    Input: nums = [1,2,1,2,2], k = 2, m = 2
+    Output: 2
+    Explanation:
+    The possible subarrays with k = 2 distinct integers, each appearing at least m = 2 times are:
+    Subarray	Distinct
+    numbers	Frequency
+    [1, 2, 1, 2]	{1, 2} → 2	{1: 2, 2: 2}
+    [1, 2, 1, 2, 2]	{1, 2} → 2	{1: 2, 2: 3}
+    Thus, the answer is 2.
+
+    Constraints:
+    1 <= nums.length <= 10^5
+    1 <= nums[i] <= 10^5
+    1 <= k, m <= nums.length
+    */
+    countSubarrays(nums: number[], k: number, m: number): number {
+        return -1
+    };
 }
