94ae823d1d5f9af0cda93645a149daeee80499cf
subh <harvirbuttar2917@gmail.com>
2026-04-04 13:04:40 -0700

    #3873: cela semble être un problème très intéressant et stimulant; je le résoudrai lors de mon prochain jour de congé

--- src/app/2026/contests/C493.py
+++ src/app/2026/contests/C493.py
@@ -60,3 +60,29 @@ class Solution:
             )
             i=j
         return min(r,n)
+    '''
+    3873. Maximum Points Activated with One Addition
+
+    You are given a 2D integer array points, where points[i] = [xi, yi] represents the coordinates of the ith point. All coordinates in points are distinct.
+    If a point is activated, then all points that have the same x-coordinate or y-coordinate become activated as well.
+    Activation continues until no additional points can be activated.
+    You may add one additional point at any integer coordinate (x, y) not already present in points. Activation begins by activating this newly added point.
+    Return an integer denoting the maximum number of points that can be activated, including the newly added point.
+
+    Example 1:
+    Input: points = [[1,1],[1,2],[2,2]]
+    Output: 4
+    Explanation:
+    Adding and activating a point such as (1, 3) causes activations:
+    (1, 3) shares x = 1 with (1, 1) and (1, 2) -> (1, 1) and (1, 2) become activated.
+    (1, 2) shares y = 2 with (2, 2) -> (2, 2) becomes activated.
+    Thus, the activated points are (1, 3), (1, 1), (1, 2), (2, 2), so 4 points in total. We can show this is the maximum activated.
+
+    Constraints:
+    1 <= points.length <= 10^5
+    points[i] = [xi, yi]
+    -10^9 <= xi, yi <= 10^9
+    points contains all distinct coordinates.
+    '''
+    def maxActivated(self, points: list[list[int]]) -> int:
+        pass
