0161f8b0b493129a7fbaaff90b78932014952e13
subh <harvirbuttar2917@gmail.com>
2026-04-15 16:37:15 -0700

    #3881: aucun problème n'est difficile, tout est soluable par la simplicité.

--- src/app/2026/contests/BWC179.py
+++ src/app/2026/contests/BWC179.py
@@ -24,39 +24,16 @@ class Solution:
     1 <= n <= 10^5
     0 <= pos, k <= n - 1
     '''
-    def __init__(self):
-        self.perm=[1]*(10**5+1)
-        self.mod=10**9+7
-        for i in range(2,10**5+1):
-            self.perm[i]=(i%self.mod * self.perm[i-1]%self.mod)%self.mod
-    def exp(self,b:int,p:int): # log(n)
-        r=1
+    def exp(self, i:int,mod:int):
+        r=1; b=i; p=mod-2
         while p>0:
-            if p%2: r=self.mul(r,b)
-            b=self.mul(b,b)
+            if p%2: r=(r*b)%mod
+            b=(b*b)%mod
             p>>=1
         return r
-    def add(self,a:int,b:int):
-        return (a%self.mod + b%self.mod)%self.mod
-    def mul(self,a:int,b:int):
-        return (a%self.mod * b%self.mod)%self.mod
-    def C(self,n,r):
-        if n<=r: return 0
-        return self.mul(
-                self.mul(
-                    self.perm[n],
-                    self.exp(self.perm[n-r], self.mod-2)
-                    ),
-                self.exp(self.perm[r],self.mod-2)
-            )
     def countVisiblePeople(self, n: int, pos: int, k: int) -> int:
-        res=0
-        if pos not in [0,n-1]:
-            for i in range(min(pos,k)):
-                res=self.add(
-                    res,
-                    self.mul(
-                        self.C(pos,i), self.C(n-pos-1,k-i)
-                    )
-                )
-        return self.mul(2,res)
+        mod=10**9+7; u=1; b=1; k=min(k,n-1-k)
+        for i in range(1,1+k):
+            u=(u*(n-i))%mod
+            b=(b*i)%mod
+        return (2*(self.exp(b,mod)*u)%mod)%mod
