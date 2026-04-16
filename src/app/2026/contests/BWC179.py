b76f976eb47da3747f347d50aa65d31039ac0d4e
subh <harvirbuttar2917@gmail.com>
2026-04-16 08:40:33 -0700

    #3882: problème résolu, mais continuez à vous entraîner

--- src/app/2026/contests/BWC179.py
+++ src/app/2026/contests/BWC179.py
@@ -37,3 +37,43 @@ class Solution:
             u=(u*(n-i))%mod
             b=(b*i)%mod
         return (2*(self.exp(b,mod)*u)%mod)%mod
+    '''
+    3882. Minimum XOR Path in a Grid
+
+    You are given a 2D integer array grid of size m * n.
+    You start at the top-left cell (0, 0) and want to reach the bottom-right cell (m - 1, n - 1).
+    At each step, you may move either right or down.
+    The cost of a path is defined as the bitwise XOR of all the values in the cells along that path,
+    including the start and end cells.
+    Return the minimum possible XOR value among all valid paths from (0, 0) to (m - 1, n - 1).
+
+    Example 1:
+    Input: grid = [[1,2],[3,4]]
+    Output: 6
+    Explanation:
+    There are two valid paths:
+    (0, 0) → (0, 1) → (1, 1) with XOR: 1 XOR 2 XOR 4 = 7
+    (0, 0) → (1, 0) → (1, 1) with XOR: 1 XOR 3 XOR 4 = 6
+    The minimum XOR value among all valid paths is 6.
+
+
+    Constraints:
+    1 <= m == grid.length <= 1000
+    1 <= n == grid[i].length <= 1000
+    m * n <= 1000
+    0 <= grid[i][j] <= 1023​
+    '''
+    def minCost(self, grid: list[list[int]]) -> int:
+        m=len(grid); n=len(grid[0])
+        dp=[[[-1]*1024 for _ in range(n)] for _ in range(m)]
+        def dfs(i:int,j:int,x:int):
+            if i>=m or j>=n: return 1023
+            x^=grid[i][j]
+            if (i,j)==(m-1,n-1): return x
+            elif dp[i][j][x]==-1:
+                right=dfs(i,j+1,x)
+                down=dfs(i+1,j,x)
+                dp[i][j][x]=min(right,down)
+            return dp[i][j][x]
+        return dfs(0,0,0)
+        
