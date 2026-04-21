896f72a3bd1a4158ed504141b0f77c0d16dcab62
subh <harvirbuttar2917@gmail.com>
2026-04-21 11:11:05 -0700

    #3890: résolu 3890, un problème basé sur les nombres <<Hardy-Ramanujan>>, cache plus recherche binaire.

--- src/app/2026/contests/C496.py
+++ src/app/2026/contests/C496.py
@@ -1,3 +1,22 @@
+
+'''#3890:cache'''
+N=10**9; x=0; k=0
+pows=[]; frq={}
+
+while k+1<=N:
+    pows.append(k)
+    x+=1; k=x**3
+
+ans=[]; m=len(pows)
+for i in range(m):
+    for j in range(i+1):
+        k=pows[i]+pows[j]
+        frq[k]=1+frq.get(k,0)
+        if frq[k]==2: 
+            ans.append(pows[i]+pows[j])
+z=len(ans); ans.sort()
+'''#3890'''
+
 class Solution:
     '''
     3890. Integers With Multiple Sum of Two Cubes
@@ -22,4 +41,9 @@ class Solution:
     1 <= n <= 10**9
     '''
     def findGoodIntegers(self, n: int) -> list[int]:
-        pass
+        l=-1;r=z
+        while l<r:
+            m=(l+r+1)//2
+            if ans[m]<=n: l=m
+            else: r=m-1
+        return ans[:l+1]
