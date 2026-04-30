080a13795bfb5de0143501b8ef63e8db5c8e8c14
subh <harvirbuttar2917@gmail.com>
2026-04-30 10:12:06 -0700

    #3898: la question finale a été posée et une solution sera bientôt trouvée.

--- src/app/2026/contests/BWC180.py
+++ src/app/2026/contests/BWC180.py
@@ -94,8 +94,8 @@ class Solution:
     nums1[i] + nums0[i] > 0
     The total sum of all elements in nums1 and nums0 does not exceed 2 * 105.
     '''
-
     def maxValue(self, nums1: list[int], nums0: list[int]) -> int:
+        #incomplete
         n=len(nums1); idx=[i for i in range(n)]
         idx.sort(key=lambda i:-(nums1[i]+nums0[i]))
         idx.sort(key=lambda i:nums0[i])
@@ -106,3 +106,28 @@ class Solution:
             res=add(res, mul(pow[l],pow[c+z]-pow[z]))
             l+=c+z
         return res
+    '''
+    3898. Find the Degree of Each Vertex
+    You are given a 2D integer array matrix of size n x n representing the adjacency matrix of an undirected graph with n vertices labeled from 0 to n - 1.
+    matrix[i][j] = 1 indicates that there is an edge between vertices i and j.
+    matrix[i][j] = 0 indicates that there is no edge between vertices i and j.
+    The degree of a vertex is the number of edges connected to it.
+    Return an integer array ans of size n where ans[i] represents the degree of vertex i.
+
+    Example 1:
+    Input: matrix = [[0,1,1],[1,0,1],[1,1,0]]
+    Output: [2,2,2]
+    Explanation:
+    Vertex 0 is connected to vertices 1 and 2, so its degree is 2.
+    Vertex 1 is connected to vertices 0 and 2, so its degree is 2.
+    Vertex 2 is connected to vertices 0 and 1, so its degree is 2.
+    Thus, the answer is [2, 2, 2].
+
+    Constraints:
+    1 <= n == matrix.length == matrix[i].length <= 100​​​​​​​
+    ​​​​​​​matrix[i][i] == 0
+    matrix[i][j] is either 0 or 1
+    matrix[i][j] == matrix[j][i]
+    '''   
+    def findDegrees(self, matrix: list[list[int]]) -> list[int]:
+        pass
