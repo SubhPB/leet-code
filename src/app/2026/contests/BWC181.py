1fbe4aebec60bb2f4876182fb0b6bd4717981507
subh <harvirbuttar2917@gmail.com>
2026-05-15 10:45:31 -0700

    #3910: nous poursuivrons le reste de la solution demain.

--- src/app/2026/contests/BWC181.py
+++ src/app/2026/contests/BWC181.py
@@ -1,3 +1,7 @@
+perm=[1]*14 #3910 @cache
+for i in range(1,14):
+    perm[i]=i*perm[i-1]
+
 class Solution:
     '''
     3909. Compare Sums of Bitonic Parts
@@ -29,3 +33,52 @@ class Solution:
         if s!=0:
             return 0 if s>0 else 1
         return -1
+    '''
+    3910. Count Connected Subgraphs with Even Node Sum
+
+    You are given an undirected graph with n nodes labeled from 0 to n - 1. Node i has a value of nums[i], which is either 0 or 1.
+    The edges of the graph are given by a 2D array edges where edges[i] = [ui, vi] represents an edge between node ui and node vi.
+    For a non-empty subset s of nodes in the graph, we consider the induced subgraph of s generated as follows
+    We keep only the nodes in s.
+    We keep only the edges whose two endpoints are both in s.
+    Return an integer representing the number of non-empty subsets s of nodes in the graph such that:
+    The induced subgraph of s is connected.
+    The sum of node values in s is even.
+
+    Example 1:
+    Input: nums = [1,0,1], edges = [[0,1],[1,2]]
+    Output: 2
+
+    Constraints:
+    1 <= n == nums.length <= 13
+    nums[i] is 0 or 1.
+    0 <= edges.length <= n * (n - 1) / 2
+    edges[i] = [ui, vi]
+    0 <= ui < vi < n
+    All edges are distinct.
+    '''
+    def evenSumSubgraphs(self, nums: list[int], edges: list[list[int]]) -> int:
+        n=len(nums); parent=[-1]*n
+        def find(x:int):
+            p=x
+            while parent[p]>-1:
+                p=parent[p]
+            if p!=x: parent[x]=p
+            return p
+        def union(x:int,y:int):
+            px=find(x); py=find(y)
+            if px!=py:
+                if parent[px]<parent[py]:
+                    parent[px]+=parent[py]
+                    parent[py]=px
+                else:
+                    parent[py]+=parent[px]
+                    parent[px]=py
+                return True
+            return False
+        for [u,v] in edges: union(u,v)
+        res=0
+        # will continue from here...
+
+        return res
+    
