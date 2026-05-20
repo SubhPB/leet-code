8da033056dfe39bed37d1681f04977759c24b06e
subh <harvirbuttar2917@gmail.com>
2026-05-19 17:18:38 -0700

    #3910: je n'ai qu'une idée générale de l'approche à adopter, mais j'ai besoin de la rendre plus efficace.

--- src/app/2026/contests/BWC181.py
+++ src/app/2026/contests/BWC181.py
@@ -91,3 +91,35 @@ class Solution:
             u=sum([C(x,i) for i in range(0,x+1,2)])
             res+=k*u-1
         return res
+    '''
+    3911. K-th Smallest Remaining Even Integer in Subarray Queries
+    You are given an integer array nums where nums is strictly increasing.
+    You are also given a 2D integer array queries, where queries[i] = [li, ri, ki].
+    For each query [li, ri, ki]:
+    Consider the subarray nums[li..ri]
+    From the infinite sequence of all positive even integers: 2, 4, 6, 8, 10, 12, 14, ...
+    Remove all elements that appear in the subarray nums[li..ri].
+    Find the kith smallest integer remaining in the sequence after the removals.
+    Return an integer array ans, where ans[i] is the result for the ith query.
+    
+    Example 1:
+    Input: nums = [1,4,7], queries = [[0,2,1],[1,1,2],[0,0,3]]
+    Output: [2,6,6]
+
+    Constraints:
+    1 <= nums.length <= 10**5
+    1 <= nums[i] <= 10**9
+    nums is strictly increasing
+    1 <= queries.length <= 10**5
+    queries[i] = [li, ri, ki]
+    0 <= li <= ri < nums.length
+    1 <= ki <= 10**9​​​​​​​
+    '''
+    def kthRemainingInteger(self, nums: list[int], queries: list[list[int]]) -> list[int]:
+        '''
+        m=len(queries)
+        Δp'= 2*ki+ 2*count(e W e<=2*ki)
+        Δp = Δp' + 2*count(e W 2*ki<e<=Δp')
+        res[i]=Δp
+        '''
+        pass
