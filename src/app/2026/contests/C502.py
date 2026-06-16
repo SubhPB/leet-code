e7e359d4ca1b6c9493d54cdbff98149dd2df9028
subh <harvirbuttar2917@gmail.com>
2026-06-15 17:01:12 -0700

    #3933: je n'ai pas eu le temps aujourd'hui j'essaierai demain.

--- src/app/2026/contests/C502.py
+++ src/app/2026/contests/C502.py
@@ -40,3 +40,34 @@ class Solution:
             else:
                 right=i-1
         return left-res
+    '''
+    3933. Largest Local Values in a Matrix II
+
+    You are given an n x m integer matrix matrix containing non-negative integers.
+    A non-zero cell (row, col) checks the cells near it as follows:
+    Let x = matrix[row][col].
+    Consider every cell within x rows and x columns of (row, col).
+    Ignore cells that are outside the matrix.
+    Ignore the cells where both the row distance and column distance are exactly x.
+    The cell (row, col) is a local maximum if it is non-zero and no considered cell has a value greater than x.
+    Return an integer denoting the number of local maximums in matrix.
+
+    ​​​​​​​Example 1:
+    Input: matrix = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,2,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
+    Output: 1
+
+    ​​​​​​​​​​​​​​​​​​​
+    Explanation:
+    For the non-zero cell (3, 3), x = matrix[3][3] = 2.
+    The highlighted cells are the considered cells within x rows and x columns of (3, 3).
+    The four cells with both row and column distances equal to x = 2 are ignored.
+    No considered cell has a value greater than 2, so (3, 3) is a local maximum.
+    There are no other non-zero cells, so the answer is 1.
+
+    Constraints:
+    1 <= n == matrix.length <= 200
+    1 <= m == matrix[i].length <= 200
+    0 <= matrix[i][j] <= 200
+    '''
+    def countLocalMaximums(self, matrix: list[list[int]]) -> int:
+        pass
