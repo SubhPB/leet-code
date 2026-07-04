d60f4a0078c1087f18402924948ad01b3b83e6f7
subh <harvirbuttar2917@gmail.com>
2026-07-03 17:12:21 -0700

    #3942: presque trouvé la solution

--- src/app/2026/contests/C503.py
+++ src/app/2026/contests/C503.py
@@ -26,3 +26,37 @@ class Solution:
             elif char in '!@#$': res+=5
             elif 0<=int(char)<=9: res+=3
         return res
+    '''
+    3942. Minimum Operations to Sort a Permutation
+    You are given an integer array nums of length n, where nums is a permutation of the integers from 0 to n - 1.
+    You may perform only the following operations:
+    Reverse the entire array.
+    Rotate Left by One: Move the first element to the end of the array, and rest elements to left by one position.
+    Return an integer denoting the minimum number of operations required to sort the array in increasing order. If it is not possible to sort the array using only the given operations, return -1.
+
+    Example 1:
+    Input: nums = [0,2,1]
+    Output: 2
+    Explanation:
+    Rotate Left by one: [2, 1, 0]
+    Reverse the array: [0, 1, 2]
+    The array becomes sorted in 2 operations, which is minimal
+
+    Constraints:
+    1 <= n == nums.length <= 10**5
+    0 <= nums[i] <= n - 1
+    nums is a permutation of integers from 0 to n - 1.
+    '''
+    def minOperations(self, nums: list[int]) -> int:
+        res=0; n=len(nums)
+        if n>1:
+            i=0; t= -1 if ((nums[0]+1)%n==nums[1]) else 1
+            print(f't={t}')
+            while i<n:
+                if nums[i]!=(nums[(i+1)%n]+t)%n:
+                    return -1
+                i+=1
+            if t<0: 
+                print(f'nums={nums}')
+                n+=1; nums[-1]=nums[0]
+        return n-1-nums[-1]
