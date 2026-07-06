964d5438390598e5cdec8a0554427203ef2be040
subh <harvirbuttar2917@gmail.com>
2026-07-05 17:04:21 -0700

    #3943: tentative avec une stratégie de <<mise à jour paresseuse>>, efficace mais insuffisante.

--- src/app/2026/contests/C503.py
+++ src/app/2026/contests/C503.py
@@ -1,3 +1,4 @@
+from collections import Counter
 class Solution:
     '''
     3941. Password Strength
@@ -60,3 +61,59 @@ class Solution:
         if i>0: #inc 
             return min(-nums[0]%n,1 + (nums[0]-n+1)%n)
         return min(-nums[-1]%n + 1,1 + (nums[0]-n+1)%n)
+    '''
+    3943. Number of Pairs After Increment
+
+    You are given two integer arrays nums1 and nums2, and a 2D integer array queries.
+    Each queries[i] is one of the following types:
+    [1, x, y, val] – Add val to every element in nums2[x..y].
+    [2, tot] – Compute the number of pairs (j, k) such that nums1[j] + nums2[k] == tot.
+    Return an integer array answer, where answer[j] is the number of pairs for the jth query of type 2.
+
+    Example 1:
+    Input: nums1 = [1,2], nums2 = [3,4], queries = [[2,5],[1,0,0,2],[2,5]]
+    Output: [2,1]
+    Explanation:
+    queries[0] = [2, 5]: Valid pairs are nums1[0] + nums2[1] = 1 + 4 = 5 and nums1[1] + nums2[0] = 2 + 3 = 5.
+    queries[1] = [1, 0, 0, 2]: Add 2 to nums2[0], resulting in nums2 = [5, 4].
+    queries[2] = [2, 5]: Valid pair is nums1[0] + nums2[1] = 1 + 4 = 5.
+    Thus, the answer = [2, 1].
+
+    Constraints:
+    1 <= nums1.length <= 5
+    1 <= nums2.length <= 5 * 10**4
+    1 <= nums1[i], nums2[i] <= 10**5
+    1 <= queries.length <= 5 * 10**4
+    queries[i].length == 2 or 4
+    queries[i] == [1, x, y, val], or
+    queries[i] == [2, tot]
+    0 <= x <= y < nums2.length
+    1 <= val <= 10**5
+    1 <= tot <= 10**9
+    '''
+    def numberOfPairs(self, nums1: list[int], nums2: list[int], queries: list[list[int]]) -> list[int]:
+        c1=Counter(nums1); c2=Counter(nums2) 
+        Δ=0; res=[]; n=len(nums2)
+        for q in queries:
+            if len(q)>2:
+                [_,l,r,val]=q
+                if r-l>=n//2:
+                    Δ+=val
+                    for i in range(l):
+                        c2[nums2[i]]-=1
+                        nums2[i]-=val
+                        c2[nums2[i]]+=1
+                    for i in range(r+1,n):
+                        c2[nums2[i]]-=1
+                        nums2[i]-=val
+                        c2[nums2[i]]+=1
+                else:
+                    for i in range(l,r+1):
+                        c2[nums2[i]]-=1
+                        nums2[i]+=val
+                        c2[nums2[i]]+=1
+            else:
+                [_,tg]=q; res.append(0)
+                for num in c1:
+                    res[-1]+=(c1.get(num,0)*c2.get(tg-num-Δ,0))
+        return res
