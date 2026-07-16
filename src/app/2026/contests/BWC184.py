1e2bf04605749a6c440b987dff6ec5b1e17b3ac4
subh <harvirbuttar2917@gmail.com>
2026-07-15 18:24:41 -0700

    #3952: sur la bonne voie mais il reste encore des cas particuliers à résoudre.

--- src/app/2026/contests/BWC184.py
+++ src/app/2026/contests/BWC184.py
@@ -39,5 +39,41 @@ class Solution:
             pstart,pend=start,max(pend,end)
             i+=1
         return t*math.ceil(brightness/3)
+    '''
+    3952. Maximum Total Value of Covered Indices
+    You are given an integer array nums of length n and a binary string s of length n, where s[i] == '1' means index i initially contains a token and s[i] == '0' means it does not.
+    You may perform the following operation any number of times:
+    Choose a token currently located at index i, where i > 0, such that this token has not been moved before.
+    Move this token from index i to index i - 1.
+    An index is considered covered if it contains a token after all moves.
+    Return an integer denoting the maximum total value of nums at the covered indices after optimally performing the operations.
 
-            
+    Example 1:
+    Input: nums = [9,2,6,1], s = "0101"
+    Output: 15
+    Explanation:
+    Initially, indices 1 and 3 contain tokens.
+    Move the token from index 3 to index 2.
+    Move the token from index 1 to index 0.
+    The covered indices are [0, 2], so the total value is nums[0] + nums[2] = 9 + 6 = 15.
+    
+    Constraints:
+    1 <= n == nums.length == s.length <= 10**5
+    1 <= nums[i] <= 10**5
+    ​​​​​​​s[i] is either '0' or '1'
+    '''
+    def maxTotal(self, nums: list[int], s: str) -> int:
+        s=list([int(x) for x in s])
+        res=s[0]*nums[0]
+        n=len(nums)
+        for i in range(1,n):
+            if s[i]:
+                if s[i-1]:
+                    res+=nums[i]
+                elif nums[i-1]>=nums[i]:
+                    res+=nums[i-1]
+                    s[i]=0; s[i-1]=1
+                else:
+                    res+=nums[i]
+        return res
+
