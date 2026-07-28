10651fc9ac053d7b496c2aad2384c373310f53cd
subh <harvirbuttar2917@gmail.com>
2026-07-28 10:32:05 -0700

    #3965: pas encore terminé, la solution actuelle répond à des question qui n'ont pas été posées. je m'en occuperai bientôt

--- src/app/2026/contests/BWC185.py
+++ src/app/2026/contests/BWC185.py
@@ -42,3 +42,42 @@ class Solution:
                 if i<n-1:
                     lights[i+1]=1 #any +ve value
         return res
+    '''
+    3965. Finish Time of Tasks I
+
+    You are given an integer n representing the number of tasks in a project, numbered from 0 to n - 1. These tasks are connected as a tree rooted at task 0.
+    This is represented by a 2D integer array edges of length n - 1, where edges[i] = [ui, vi] indicates that task ui is the parent of task vi.
+    You are also given an array baseTime of length n, where baseTime[i] represents the time to complete task i.
+    The finish time of each task is calculated as follows:
+    Leaf task: The finish time is baseTime[i].
+    Non-leaf task:
+    Let earliest be the minimum finish time among its children, and latest be the maximum finish time among its children.
+    Let ownDuration be (latest - earliest) + baseTime[i].
+    The finish time of task i is latest + ownDuration.
+    Return the finish time of the root task 0.
+
+    Example 1:
+    Input: n = 3, edges = [[0,1],[1,2]], baseTime = [9,5,3]
+    Output: 17
+
+    Constraints:
+    1 <= n <= 10**5
+    edges.length = n - 1
+    edges[i] == [ui, vi]
+    0 <= ui, vi <= n - 1
+    ui != vi
+    1 <= baseTime[i] <= 10**5​​​​​​​
+    The finish time of every task is guaranteed to be less than 2**53.
+    '''
+    def finishTime(self, n: int, edges: list[list[int]], baseTime: list[int]) -> int:
+        if n<=1: return baseTime[0] 
+        latest,earliest=[baseTime[1]]*2
+        for [u,v] in edges:
+            if u!=0:
+                latest=max(latest,baseTime[u],baseTime[v])
+                earliest=min(latest,baseTime[u],baseTime[v])
+            else:
+                latest=max(latest,baseTime[v])
+                earliest=min(latest,baseTime[v])
+        return 2*latest-earliest+baseTime[0]
+
