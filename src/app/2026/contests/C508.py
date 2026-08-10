1d1a4eb167c574610827f24b1203561c11a37931
subh <harvirbuttar2917@gmail.com>
2026-08-09 22:12:52 -0700

    #3976: j'ai commencé a formuler le problème, je le ferai demain.

--- src/app/2026/contests/C508.py
+++ src/app/2026/contests/C508.py
@@ -53,3 +53,30 @@ class Solution:
                 # elif e< will continue
             
         return res
+    '''
+    3976. Maximum Subarray Sum After Multiplier
+
+    You are given an integer array nums and a positive integer k.
+    You must choose exactly one subarray of nums and perform exactly one of the following operations:
+    Multiply each number in the chosen subarray by k.
+    Divide each number in the chosen subarray by k.
+    When dividing a positive number by k, use the floor value of the division result.
+    When dividing a negative number by k, use the ceiling value of the division result.
+    Return the maximum possible sum of a non-empty subarray in the resulting array.
+    Note that the subarray chosen for the operation and the subarray chosen for the sum may be different.
+
+    Example 1:
+    Input: nums = [1,-2,3,4,-5], k = 2
+    Output: 14
+    Explanation:
+    Multiply each number in the subarray [3, 4] by 2.
+    This results in nums = [1, -2, 6, 8, -5].
+    The subarray with the largest sum is [6, 8], so the output is 6 + 8 = 14.
+
+    Constraints:
+    1 <= nums.length <= 10**5
+    -105 <= nums[i] <= 10**5
+    1 <= k <= 10**5
+    '''
+    def maxSubarraySum(self, nums: list[int], k: int) -> int:
+        pass
