4643b8115685a50ec93b8cd2450623a37328c06d
subh <harvirbuttar2917@gmail.com>
2026-08-16 13:46:57 -0700

    #3980: je pense que je j'avais mal compris l'énoncé de problème

--- src/app/2026/contests/BWC186.py
+++ src/app/2026/contests/BWC186.py
@@ -23,3 +23,31 @@ class Solution:
         for j in range(n-1,k-1,-1):
             res=max(res,nums[j]+mxr[j-k])
         return res
+    '''
+    3980. Minimum Operations to Transform Binary String
+
+    You are given two binary strings s1 and s2 of the same length n.
+    You can perform the following operations on s1 any number of times, in any order:
+    Choose an index i such that s1[i] == '0', and change it to '1'.
+    Choose an index i such that 0 <= i < n - 1, and both s1[i] and s1[i + 1] are '1'. Change both characters to '0'.
+    Return the minimum number of operations required to make s1 equal to s2. If it is impossible, return -1.
+
+    Example 1:
+    Input: s1 = "11", s2 = "00"
+    Output: 1
+    Explanation:
+    Change indices 0 and 1 from '1' to '0' in one operation, so "11" becomes "00". Thus, the answer is 1.
+
+    Constraints:
+    1 <= n == s1.length == s2.length <= 10**5
+    s1 and s2 consist only of '0' and '1'.
+    '''
+    def minOperations(self, s1: str, s2: str) -> int:
+        res=0; i=0; n=len(s1)
+        while i<n:
+            if s1[i]!=s2[i]:
+                res+=1
+                if i+1<n and s1[i+1]!=s2[i+1] and (s1[i]==s1[i+1]=='1'):
+                    i+=1       
+            i+=1
+        return res
