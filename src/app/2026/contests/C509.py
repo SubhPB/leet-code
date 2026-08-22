fa970fb521bb91d3fd94bb0b8c2957eeb9e46dbb
subh <harvirbuttar2917@gmail.com>
2026-08-22 11:59:51 -0700

    #3983: j'ai changé d'approche et je vais poursuivre à partir de là.

--- src/app/2026/contests/C509.py
+++ src/app/2026/contests/C509.py
@@ -18,50 +18,21 @@ class Solution:
     s and t consist only of lowercase English letters.
     '''
     def canMakeSubsequence(self, s: str, t: str) -> bool:
-        n=len(s); m=len(t)
-        i=0; j=0
+        n=len(s)
+        m=len(t)
+        left=0
+        for ct in t:
+            if ct==s[left]:
+                left+=1
+            if left==n: return True
 
-        dt={}
-        for ch in t: dt[ch]=1+dt.get(ch,0)
-
-        used=0
-        curr={}
-        while i<n and j<m:
-            ch=s[i]; ct=t[i]
-            if ch not in curr:
-                curr[ch]=0
-
-            if ch==ct:
-                curr[ch]+=1
-                i+=1
-            else:
-                # prévoir, si 'ch' existe dans 't' reste
-                cnt=dt.get(ch,0)-curr[ch]
-                if cnt<=0:
-                    used+=1
-                    i+=1
-
-            if used>1: return False #un change nécessaire
-            j+=1
-
-        if i==n: return True
-
-        # même intuition mais à l'envers
-        used=0
-        curr={}
-        i=n-1; j=m-1
-        while j>=0 and i>=0:
-            ch=s[i]; ct=t[j]
-            if ch not in curr: 
-                curr[ch]=0
-            if ch==ct:
-                curr[ch]+=1
-                i-=1
-            else:
-                cnt=dt.get(ch,0)-curr[ch]
-                if cnt<=0:
-                    used+=1
-                    i-=1
-            if used>1: return False
-            j-=1
-        return i==-1
+        # remplacer nécessaire
+        right=n-1
+        for i in range(n-1,-1,-1):
+            ct=t[i]; ch=s[right]
+            if ct==ch: right-=1
+        
+        right+=1; left-=1 # les rendre inclusifs
+        for i,ch in enumerate(s):
+            pass
+        return False
