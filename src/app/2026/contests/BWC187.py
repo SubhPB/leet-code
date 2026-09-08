e7915c74384b7cfc86502327cf9ce5ce9a6549a4
subh <harvirbuttar2917@gmail.com>
2026-09-07 22:50:50 -0700

    #3994: initialisé

--- src/app/2026/contests/BWC187.py
+++ src/app/2026/contests/BWC187.py
@@ -27,3 +27,29 @@ class Solution:
         if not n-1: return s
         fn = lambda i:s - (i//2) + m*((i+1)//2 )
         return max(fn(n-1), fn(n-2)) 
+    '''
+    3994. Minimum Adjacent Swaps to Partition Array
+
+    You are given an integer array nums and two integers a and b such that a < b.
+    An array is called good if it can be split into three contiguous parts, in this order, such that:
+    Every element in the first part is less than a.
+    Every element in the second part is in the range [a, b] inclusive.
+    Every element in the third part is greater than b.
+    Any of the three parts may be empty.
+    In one adjacent swap, you may swap two neighboring elements of nums.
+    Return the minimum number of adjacent swaps required to make nums good. Since the answer may be very large, return it modulo 109 + 7.
+
+    Example 1:
+    Input: nums = [1,3,2,4,5,6], a = 3, b = 4
+    Output: 1
+    Explanation:
+    Swap nums[1] and nums[2]. The array becomes [1, 2, 3, 4, 5, 6].
+    This array is good because it can be split into [1, 2], [3, 4], and [5, 6].
+
+    Constraints:
+    1 <= nums.length <= 10**5
+    ​​​​​​​1 <= nums[i] <= 10**9
+    1 <= a < b <= 10**9
+    '''
+    def minAdjacentSwaps(self, nums: list[int], a: int, b: int) -> int:
+        pass
