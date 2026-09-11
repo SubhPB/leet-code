b0124cca1620768bef0bf16024777fed01dedad6
subh <harvirbuttar2917@gmail.com>
2026-09-11 10:36:05 -0700

    cela semble être un problème facile

---
+++ src/app/2026/contests/C511.py
@@ -0,0 +1,15 @@
+class Solution:
+    '''
+    3997. Count Dominant Nodes in a Binary Tree
+
+    You are given the root of a complete binary tree.
+    A node x is called dominant if its value is equal to the maximum value among all nodes in the subtree rooted at x.
+    Return the number of dominant nodes in the tree.
+
+    Constraints:
+    The number of nodes in the tree is in the range [1, 10**5].
+    1 <= Node.val <= 10**9
+    The tree is guaranteed to be a complete binary tree.
+    '''
+    def countDominantNodes(self, root: TreeNode | None) -> int:
+        pass
