985a25adb0fe5f55ef4f795eec6d96bc90fa837e
subh <harvirbuttar2917@gmail.com>
2026-09-13 11:33:48 -0700

    #3998: dans la bonne direction, dans une certaine mesure; une petite erreur à corriger

--- src/app/2026/contests/C511.py
+++ src/app/2026/contests/C511.py
@@ -29,3 +29,46 @@ class Solution:
             return max(node.val,lval,rval)
         fn(root)
         return res
+    '''
+    3998. Transform Binary String Using Subsequence Sort
+
+    You are given a binary string s.
+    You are also given an array of strings strs, where each strs[i] has the same length as s and consists of characters '0', '1', and '?'.
+    Each '?' can be replaced by either '0' or '1'.
+
+    You may perform the following operation any number of times (including zero):
+    Choose any subsequence sub of s.
+    Sort sub in non-decreasing order.
+    Replace the chosen subsequence in s with the sorted sub, keeping all other characters unchanged.
+    Return a boolean array ans, where ans[i] is true if it's possible to replace all '?' in strs[i] with '0' or '1'
+    and transform s into the resulting string using the allowed operation above, otherwise return false.
+
+
+    Example 1:
+    Input: s = "101", strs = ["1?1","0?1","0?0"]
+    Output: [true,true,false]
+
+    Explanation:
+    i	strs[i]	Replacement	Result strs[i]	Operation(s)	Result
+    0	"1?1"	? → 0	"101"	Matches s.	true
+    1	"0?1"	? → 1	"011"	Select the subsequence at indices [0..2] of s → "101".
+    Sort "101" to get "011" = strs[i].	true
+    2	"0?0"	? → 0 or 1	"000" or "010"	Not feasible.	false
+    Thus, ans = [true, true, false].
+
+    Constraints:
+    1 <= n == s.length <= 2000
+    s[i] is either '0' or '1'.
+    1 <= strs.length <= 2000
+    strs[i].length == n
+    strs[i] is either '0', '1', or '?'​​​​​​​.
+    '''
+    def transformStr(self, s: str, strs: list[str]) -> list[bool]:
+        cnt1=s.count('1')
+        for i,st in enumerate(strs):
+            cnt2=st.count('1')
+            if cnt1>=cnt2:
+                strs[i]=cnt2+st.count('?')>=cnt1
+            else:
+                strs[i]=False
+        return strs
